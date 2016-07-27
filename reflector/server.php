144e58d34a9240f3b31c3afc52a6415230a5135c
Jason Rohrer <jasonrohrer@fastmail.fm>
2016-07-27 11:24:00 -0700

    Made test server address absolute.

--- reflector/server.php
+++ reflector/server.php
@@ -3,7 +3,7 @@
 
 // for now, keep reflector server as simple as possible
 
-echo "127.0.0.01\n";
+echo "192.168.1.3\n";
 echo "8005\n";
 echo "1\n";
 echo "http://dummy1238438490.com/server.php\n";
