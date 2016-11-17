0d7ebe59263bff48e4892a5119f1ceea26a9a1e1
Jason Rohrer <jasonrohrer@fastmail.fm>
2016-11-17 15:17:27 -0800

    First website.

---
+++ documentation/html/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<title>One Hour One Life</title>
+</head>
+
+<body bgcolor=black text=white link=#91ac66 vlink=#91ac66 alink=#91ac66>
+
+<center>
+
+<img border=0 width=448 height=360 src="logo448x360.png">
+<br><br>
+a multiplayer game of parenting and civilization building by <a href=http://hcsoftware.sf.net/jason-rohrer>Jason Rohrer</a> and <a href=http://delcroy.com>Tom Bailey</a>
+
+</center>
+<br>
+<br>
+
+</body>
+
+</html>
