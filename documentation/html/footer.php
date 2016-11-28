89d1d79fd89080aecf5084efa903742619c5511c
Jason Rohrer <jasonrohrer@fastmail.fm>
2016-11-28 14:00:19 -0800

    Fixed Tom link.  Added sign-up form.  Added embedded video.

---
+++ documentation/html/footer.php
@@ -0,0 +1,28 @@
+<br>
+<center>
+
+<?php
+
+$nocounter = "0";
+if( isset( $_REQUEST[ "nocounter" ] ) ) {
+    $nocounter = $_REQUEST[ "nocounter" ];
+    }
+
+if( ! $nocounter && ( !isSet( $blockCounter ) || ! $blockCounter ) ) {
+?>
+
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(101006849); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101006849ns.gif" /></p></noscript> 
+
+<?php
+     }
+?>
+
+     
+</center>
+
+
+</body>
+
+</html>
--- documentation/html/index.php
+++ documentation/html/index.php
@@ -10,12 +10,28 @@
 
 <img border=0 width=448 height=360 src="logo448x360.png">
 <br><br>
-a multiplayer game of parenting and civilization building by <a href=http://hcsoftware.sf.net/jason-rohrer>Jason Rohrer</a> and <a href=http://delcroy.com>Tom Bailey</a>
+a multiplayer game of parenting and civilization building by <a href=http://hcsoftware.sf.net/jason-rohrer>Jason Rohrer</a> and <a href=https://soundcloud.com/delcroy>Tom Bailey</a>
+
+<br>
+<br>
+
+<iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/uYjm3XMrqNo?rel=0" frameborder="0" allowfullscreen></iframe>
+<br>
+<br>
+
+<form action="http://northcountrynotes.org/releaseList/server.php" 
+      method="post">
+<input type="hidden" name="action" value="subscribe">
+Sign up for release announcement emails: <input type="text" name="email" value="">
+<input type="submit" value="Subscribe"><br>
+(A few brief emails a year, at most.)
+</form>
+<br>
+
+<br>
 
 </center>
 <br>
 <br>
 
-</body>
-
-</html>
+<?php include( "footer.php" ); ?>
