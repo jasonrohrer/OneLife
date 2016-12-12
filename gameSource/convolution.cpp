b9b9118bc4967d98a2f6b0c4bd7b1a4521f03146
Jason Rohrer <jasonrohrer@fastmail.fm>
2016-12-12 12:21:26 -0800

    Got windowed convolution working.

--- gameSource/convolution.cpp
+++ gameSource/convolution.cpp
@@ -4,6 +4,8 @@
 #include "minorGems/system/Time.h"
 
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 
 static void naiveConvolve( double *inA, int inLengthA,
@@ -19,6 +21,57 @@ static void naiveConvolve( double *inA, int inLengthA,
 
 
 
+static double *zeroPad( double *inSource, 
+                        int inSourceLength, int inNewLength ) {
+    double *padded = new double[ inNewLength ];
+    
+    for( int i=0; i<inNewLength; i++ ) {
+        padded[i] = 0.0;
+        }
+    memcpy( padded, inSource, sizeof( double ) * inSourceLength );
+
+    return padded;
+    }
+
+
+
+static void windowConvolve( int inWindowSize,
+                            double *inA, int inLengthA,
+                            double *inB, int inLengthB,
+                            double *inDest ) {
+    int windowsA = lrint( ceil( inLengthA / (double)inWindowSize ) );
+    int windowsB = lrint( ceil( inLengthB / (double)inWindowSize ) );
+
+
+    double *paddedA = zeroPad( inA, inLengthA, windowsA * inWindowSize );
+    double *paddedB = zeroPad( inB, inLengthB, windowsB * inWindowSize );
+    
+    double *paddedDest = zeroPad( inDest, inLengthA + inLengthB,
+                                  windowsA * inWindowSize +
+                                  windowsB * inWindowSize );
+    
+    for( int a=0; a<windowsA; a++ ) {
+        int offsetA = a * inWindowSize;
+        for( int b=0; b<windowsB; b++ ) {
+            int offsetB = b * inWindowSize;
+            
+            int destOffset = offsetA + offsetB;
+            
+            naiveConvolve( &( inA[offsetA] ), inWindowSize,
+                           &( inB[offsetB] ), inWindowSize,
+                           &( paddedDest[ destOffset ] ) );
+            }
+        }
+    
+    memcpy( inDest, paddedDest, sizeof( double ) * ( inLengthA + inLengthB ) );
+    
+    delete [] paddedA;
+    delete [] paddedB;
+    delete [] paddedDest;
+    }
+
+
+
 
 void convolve( double *inA, int inLengthA,
                double *inB, int inLengthB,
@@ -26,7 +79,8 @@ void convolve( double *inA, int inLengthA,
     
     double start = Time::getCurrentTime();
     
-    naiveConvolve( inA, inLengthA, inB, inLengthB, inDest );
+    //    naiveConvolve( inA, inLengthA, inB, inLengthB, inDest );
+    windowConvolve( 512, inA, inLengthA, inB, inLengthB, inDest );
     
     printf( "Convolution of %dx%d took %.3f seconds\n",
             inLengthA, inLengthB, Time::getCurrentTime() - start );
