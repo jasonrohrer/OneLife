fe7b0a646a819ed6e2cdb8fa82dbb221d8f926ea
Jason Rohrer <jasonrohrer@fastmail.fm>
2016-12-14 12:20:03 -0800

    Moved header html to separate file.  Started a credit tracking page.

---
+++ documentation/html/credits.php
@@ -0,0 +1,9 @@
+<?php include( "header.php" ); ?>
+
+
+FFT code by <a href="http://www.kurims.kyoto-u.ac.jp/~ooura/fft.html">Takuya Ooura</a>.<br><br>
+
+OGG Vorbis decoder by <a href="http://nothings.org/stb_vorbis/">Sean Barrett</a?.<br><br>
+
+
+<?php include( "footer.php" ); ?>
--- documentation/html/index.php
+++ documentation/html/index.php
@@ -1,19 +1,4 @@
-<html>
-
-<head>
-<title>One Hour One Life</title>
-</head>
-
-<body bgcolor=black text=white link=#91ac66 vlink=#91ac66 alink=#91ac66>
-
-<center>
-
-<img border=0 width=448 height=360 src="logo448x360.png">
-<br><br>
-a multiplayer game of parenting and civilization building by <a href=http://hcsoftware.sf.net/jason-rohrer>Jason Rohrer</a> and <a href=https://soundcloud.com/delcroy>Tom Bailey</a>
-
-<br>
-<br>
+<?php include( "header.php" ); ?>
 
 <iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/uYjm3XMrqNo?rel=0" frameborder="0" allowfullscreen></iframe>
 <br>
