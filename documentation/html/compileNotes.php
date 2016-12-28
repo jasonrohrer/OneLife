33a3973dfce3612f8d5a8aa3ea32ccb7cce0541f
Jason Rohrer <jasonrohrer@fastmail.fm>
2016-12-28 12:07:09 -0800

    Added compile notes and no-bot and no-counter header and footer.

---
+++ documentation/html/compileNotes.php
@@ -0,0 +1,15 @@
+<?php include( "header.php" ); ?>
+
+<font size=6>Compiling from source</font><br>
+<table border=0 width="100%" cellpadding=5><tr><td bgcolor="#222222">
+Make sure you have the <b>dev</b> package of the following library installed:
+<ul>
+<li>libsdl
+</ul>
+The included "runToBuild" script should build the game for you automatically.
+
+</td></tr></table>
+<br>
+<br>
+
+<?php include( "footer.php" ); ?>
--- documentation/html/header.php
+++ documentation/html/header.php
@@ -2,6 +2,19 @@
 
 <head>
 <title>One Hour One Life</title>
+
+<?php
+global $pathToRoot;
+
+
+if( isset( $blockRobots ) && $blockRobots == 1 ) {
+?>
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="googlebot" content="noindex, nofollow">
+<?php
+    }
+?>
+
 </head>
 
 <body bgcolor=black text=white link=#91ac66 vlink=#91ac66 alink=#91ac66>
