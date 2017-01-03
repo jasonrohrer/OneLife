cfce1df51a7ffa3a7314884d0154a8089ab99560
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-01-03 13:59:42 -0800

    Cron job script for auto-generating object report for homepage.

---
+++ scripts/generateObjectReport.sh
@@ -0,0 +1,18 @@
+cd /home/jcr15
+
+cd checkout/OneLifeData6Latest
+
+hg pull
+hg update
+
+cd ../OneLifeWorking/gameSource
+
+hg pull
+hg update
+
+
+sh makePrintReportHTML
+
+cd ../../OneLifeData6Latest
+
+../../OneLifeWorking/gameSource/printReportHTML ../../public_html/objectReport.php
