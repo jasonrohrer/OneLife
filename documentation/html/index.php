494448e8b3324accb6aee409e1efafc119129945
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-01-03 13:58:57 -0800

    Added placeholder objects report for cron job.

--- documentation/html/index.php
+++ documentation/html/index.php
@@ -2,6 +2,9 @@
 
 <center>
 
+<?php include( "objectsReport.php" ); ?>
+
+
 <img border=0 width=448 height=360 src="logo448x360.png">
 <br><br>
 a multiplayer game of parenting and civilization building by <a href=http://hcsoftware.sf.net/jason-rohrer>Jason Rohrer</a> and <a href=https://soundcloud.com/delcroy>Tom Bailey</a>
