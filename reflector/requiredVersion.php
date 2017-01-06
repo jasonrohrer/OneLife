7aa9be8b1f77842b36534d2fd194dceb4e8dc3c3
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-01-06 13:32:00 -0800

    Moved version number into config file.

---
+++ reflector/requiredVersion.php
@@ -0,0 +1,1 @@
+<?php $version=0; ?>
--- reflector/server.php
+++ reflector/server.php
@@ -1,11 +1,14 @@
 <?php
 
+include( "requiredVersion.php" );
+
+global $version;
 
 // for now, keep reflector server as simple as possible
 
 echo "192.168.1.124\n";
 echo "8005\n";
-echo "0\n";
+echo "$version\n";
 echo "http://192.168.1.124/jcr13/updateServer/server.php\n";
 echo "OK";
 
