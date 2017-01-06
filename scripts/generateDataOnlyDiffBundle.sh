454ac5ea410514b197a982e125d7efe5cfb2806a
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-01-06 13:55:14 -0800

    Working on script to auto-generate diff bundles.

---
+++ scripts/generateDataOnlyDiffBundle.sh
@@ -0,0 +1,203 @@
+
+echo "" 
+echo "Updating minorGems"
+echo ""
+
+cd ~/checkout/minorGems
+hg pull
+hg update
+
+
+
+echo "" 
+echo "Building diffBundle tool"
+echo ""
+
+cd game/diffBundle
+./diffBundleCompile
+
+
+
+echo "" 
+echo "Updating OneLifeData6Latest"
+echo ""
+
+cd ~/checkout/OneLifeData6Latest
+hg pull
+hg update
+
+
+
+lastTaggedDataVersion=`hg tags | grep "OneLife" -m 1 | awk '{print $1}' | sed -e 's/OneLife_v//'`
+
+
+echo "" 
+echo "Most recent Data hg version is:  $lastTaggedDataVersion"
+echo ""
+
+
+
+numNewChangsets=`hg log -P OneLife_v$lastTaggedDataVersion | grep changeset: | wc -l`
+
+
+echo "" 
+echo "Num hg revisions since version:  $numNewChangsets"
+echo ""
+
+
+
+if [ $numNewChangsets -lt 2 ]
+then
+	echo "Too few revisions to bundle (only tag revision), exiting."
+	exit 1
+fi
+
+
+
+echo "" 
+echo "Updating OneLifeWorking"
+echo ""
+
+cd ~/checkout/OneLifeWorking
+hg pull
+hg update
+
+
+lastTaggedCodeVersion=`hg tags | grep "OneLife" -m 1 | awk '{print $1}' | sed -e 's/OneLife_v//'`
+
+
+echo "" 
+echo "Most recent Code hg version is:  $lastTaggedCodeVersion"
+echo ""
+
+
+lastTaggedVersion=$lastTaggedDataVersion
+
+if [ $lastTaggedCodeVersion -gt $lastTaggedDataVersion ]
+then
+	lastTaggedVersion=$lastTaggedCodeVersion
+fi
+
+
+echo "" 
+echo "Most recent Data or Code hg version is:  $lastTaggedVersion"
+echo ""
+
+
+
+newVersion=$lastTaggedVersion + 1
+
+echo "" 
+echo "Using new version number:  $newVersion"
+echo ""
+
+
+
+cd ~/checkout/OneLifeData6Latest
+
+
+mkdir ~/checkout/diffWorking
+
+
+echo "" 
+echo "Exporing latest data for diffing"
+echo ""
+
+
+hg archive ~/checkout/diffWorking/dataLatest
+rm ~/checkout/diffWorking/dataLatest/.hg*
+
+
+
+echo "" 
+echo "Exporing last tagged data for diffing"
+echo ""
+
+hg update -r OneLife_v$lastTaggedDataVersion
+
+hg archive ~/checkout/diffWorking/dataLast
+rm ~/checkout/diffWorking/dataLast/.hg*
+
+
+# restore repository to latest, to avoid confusion later
+hg update
+
+
+
+echo "" 
+echo "Generating diff bundle"
+echo ""
+
+cd ~/checkout/diffWorking
+
+dbzFileName=${newVersion}_inc_all.dbz
+
+~/checkout/minorGems/game/diffBundle/diffBundle dataLast dataLatest $dbzFileName
+
+mv $dbzFileName ~/www/updateBundles/
+
+echo -n "http://onehouronelife.com/updateBundles/$dbzFileName" ~/diffBundles/${newVersion}_inc_all_urls.txt
+
+
+rm -r ~/checkout/diffWorking
+
+
+echo "" 
+echo "Shutting down server"
+echo ""
+
+serverPID=`pgrep OneLifeServer`
+
+if [ -z $serverPID ]
+then
+	echo "Server not running!"
+else
+	echo -n "1" > ~/checkout/OneLife/server/settings/shutdownMode.ini
+
+	echo "" 
+	echo "Set server shutdownMode, waiting for server to exit"
+	echo ""
+	wait $serverPID
+fi
+
+
+echo "" 
+echo "Updating live server data"
+echo ""
+
+
+cd ~/checkout/OneLifeData6
+hg pull
+hg update
+
+
+
+newTag=OneLife_v$newVersion
+
+echo "" 
+echo "Tagging live server data with new hg tag $newTag"
+echo ""
+
+hg tag $newTag
+
+
+echo "" 
+echo "Pushing tag change to central hg server"
+echo ""
+
+hg push
+
+
+
+echo "" 
+echo "Re-launching server"
+echo ""
+
+
+echo -n "0" > ~/checkout/OneLife/server/settings/shutdownMode.ini
+
+
+
+cd ~/checkout/OneLife/server/
+
+sh ./runHeadlessServerLinux.sh
