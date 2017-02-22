27e0094bbf7925bf6970c0522ccdb7c3dccab3b4
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-02-22 13:11:05 -0800

    Fixed to hash password on both client and server so that stealing stored has from server doesn't grant remote access.  Improved user flow so that we don't have to type password for each change.

--- documentation/html/artLogUpload.php
+++ documentation/html/artLogUpload.php
@@ -16,35 +16,48 @@ include( "artLogSettings.php" );
 
 $files = array();
 $thumbs = array();
-$dir = opendir('artPosts');
+$numFiles = 0;
+$numThumbs = 0;
 
-if( $dir === false ) {
-    echo "Report failed\n";
-    exit();
+
+
+function rescanFiles() {
+    global $files, $thumbs, $numThumbs, $numFiles;
+
+    $files = array();
+    $thumbs = array();
+    
+    $dir = opendir('artPosts');
+
+    if( $dir === false ) {
+        echo "Report failed\n";
+        exit();
+        }    
+
+    while( false !== ( $file = readdir($dir) ) ) {
+        if( ($file != ".") and ($file != "..") ) {
+            if( ! preg_match( "/[0-9]+_t.jpg/", $file ) ) {
+                $files[] = $file;
+                }
+            else {
+                $thumbs[] = $file;
+                }
+            }   
+        }
+
+    natsort( $files );
+    natsort( $thumbs );
+    
+    $files = array_reverse( $files );
+    $thumbs = array_reverse( $thumbs );
+
+    $numFiles = sizeof( $files );
+    $numThumbs = sizeof( $thumbs );
     }
 
 
+rescanFiles();
 
-
-while( false !== ( $file = readdir($dir) ) ) {
-    if( ($file != ".") and ($file != "..") ) {
-        if( ! preg_match( "/[0-9]+_t.jpg/", $file ) ) {
-            $files[] = $file;
-            }
-        else {
-            $thumbs[] = $file;
-            }
-        }   
-    }
-
-natsort( $files );
-natsort( $thumbs );
-
-$files = array_reverse( $files );
-$thumbs = array_reverse( $thumbs );
-
-$numFiles = sizeof( $files );
-$numThumbs = sizeof( $thumbs );
 
 
 $upload = 0;
@@ -53,116 +66,36 @@ if( isset( $_REQUEST[ "upload" ] ) ) {
     }
 
 
-if( !$upload ) {
-    // show form
 
 global $passwordHashingPepper;
 
-?>
+
+echo "<body>";
 
 
-<script type="text/javascript" src="sha1.js"></script>
-
-<script type="text/javascript">
-
-
-  function calcHMAC() {
-      try {
-  
-          var hmacInput = document.getElementById("hmacInputText");
-          var pepperInput = document.getElementById("pepperInputText");
-          var hmacOutput = document.getElementById("hmacOutputText");
-  
-          var shaObj = new jsSHA("SHA-1", "TEXT");
-          shaObj.setHMACKey( pepperInput.value, "TEXT" );
-          shaObj.update( hmacInput.value );
-          hmacOutput.value = shaObj.getHMAC("HEX");
-
-          } catch(e) {
-              hmacOutput.value = e.message
-          }
-      }
-</script>
-
-<body onload="calcHMAC()">
-
-<FORM>
-      Password:
-    <INPUT TYPE="password" MAXLENGTH=20 SIZE=20 NAME="password" autofocus id="hmacInputText" onkeyup="calcHMAC()">
-</FORM>
-
-
-
-<FORM enctype="multipart/form-data" ACTION="artLogUpload.php" METHOD="post">
-
-
-  Top image:<br>
-  <table border=1 cellpadding=5 cellspacing=0>
-	<tr>
-<?php
-
-      for( $i=0; $i<$numThumbs; $i++ ) {
-          $t = $thumbs[$i];
-
-          preg_match( "/([0-9]+)_t.jpg/", $t, $matches );
-
-          $num = $matches[1];
-          
-          echo "<td align=center valign=bottom><img src='artPosts/$t'><br>\n";
-          $checked = "";
-          if( $i == 0 ) {
-              $checked = "checked";
-              }
-          echo "<input type=radio name=newTop value=$num $checked>\n";
-          echo "</td>\n";
-
-          if( $i > 0 && $i % 6 == 0 ) {
-              echo "</tr><tr>";
-              }
-          }
-?>
-
-</tr></table>
-
-    <INPUT TYPE="hidden" NAME="upload" VALUE="1">
-
-<br>
-<br>
-Server-provided Pepper:
-<br>
-	<input type="text" size="75" name="pepper" readonly value="<?php echo $passwordHashingPepper;?>" id="pepperInputText">
-
-    <br>
-hmac_sha1 of password with pepper as key:<br>
-	<input type="text" size="75" name="passwordHMAC" id="hmacOutputText">
-
-      <br><br><br><br>
-      Post this image file: <input name="userfile" type="file" />
-      <br>
-      <br>
-       <INPUT TYPE="Submit" VALUE="Upload">
-
-      
-    </FORM>
-
-
-</body>
-
-
-<?php
-      
-    }
-else {
+if( $upload ) {
+        
     // trying to upload
 
     global $accessPasswords, $passwordHashingPepper;
     
     $loginPermitted = false;
+
+    $passwordSubmittedHash = "";
     
     if( isset( $_REQUEST[ "passwordHMAC" ] ) ) {
-        $password = $_REQUEST[ "passwordHMAC" ];
+        $passwordSubmittedHash = $_REQUEST[ "passwordHMAC" ];
+
+        preg_match( "/[0-9a-f]+/", $passwordSubmittedHash, $matches );
+        $passwordSubmittedHash = $matches[0];
+
         
-        if( in_array( $password, $accessPasswords ) ) {
+        
+        $passwordDoubleHash = hash_hmac( "sha1",
+                                         $passwordSubmittedHash,
+                                         $passwordHashingPepper );
+        
+        if( in_array( $passwordDoubleHash, $accessPasswords ) ) {
             $loginPermitted = true;
             }
         }
@@ -209,9 +142,6 @@ else {
             }
         
         $lastFileNumber = $newTopNumber;
-        
-        echo "New top image selected: ".
-            "<img src='artPosts/$newTopNumber"."_t.jpg'><br><br>\n";
         }
     
 
@@ -224,52 +154,163 @@ else {
     
     $ext = pathinfo( $originalFileName, PATHINFO_EXTENSION );
 
-    if( $ext != "jpg" && $ext != "png" ) {
-        echo "Only JPG and PNG files supported.";
+    if( $ext != "" && 
+        $ext != "jpg" && $ext != "png" ) {
 
-        
-        echo "<br><br><a href=artLogUpload.php>Upload more</a><br><br>";
-        
-        echo "<a href=artLogPage.php>Go to art log</a>";
+        echo "Only JPG and PNG files supported, not '$ext'";
+        }
+    else if( $ext != "" ) {    
+    
+        $newFileNumber = $lastFileNumber + 1;
 
-        die();
+        $path =
+            substr( $_SERVER["SCRIPT_FILENAME"],
+                    0, strpos( $_SERVER["SCRIPT_FILENAME"],
+                               "/artLogUpload.php" ) );
+    
+        $bigFile = "artPostsBig/$newFileNumber.$ext";
+
+        echo "Renaming $tmpFileName to $bigFile <br>";
+    
+        rename( $tmpFileName, $bigFile );
+        chmod( $bigFile, 0666 );
+
+        $postFile = "artPosts/$newFileNumber.jpg";
+        $postFileThumb = "artPosts/$newFileNumber"."_t.jpg";
+
+        shell_exec( "convert -resize '600>' -quality 92 $bigFile $postFile" );
+        shell_exec(
+            "convert -resize '80>' -quality 92 $bigFile $postFileThumb" );
+
+        echo "Converting to small display versions at ".
+            "$postFile, $postFileThumb<br>";
+
+        chmod( $postFile, 0666 );
+        chmod( $postFileThumb, 0666 );
+    
+        echo "Upload complete.<br><br>";
+        
         }
     
-    
-    
-    
-    $newFileNumber = $lastFileNumber + 1;
-
-    $path =
-        substr( $_SERVER["SCRIPT_FILENAME"],
-                0, strpos( $_SERVER["SCRIPT_FILENAME"], "/artLogUpload.php" ) );
-    
-    $bigFile = "artPostsBig/$newFileNumber.$ext";
-
-    echo "Renaming $tmpFileName to $bigFile <br>";
-    
-    rename( $tmpFileName, $bigFile );
-    chmod( $bigFile, 0666 );
-
-    $postFile = "artPosts/$newFileNumber.jpg";
-    $postFileThumb = "artPosts/$newFileNumber"."_t.jpg";
-
-    shell_exec( "convert -resize '600>' -quality 92 $bigFile $postFile" );
-    shell_exec( "convert -resize '80>' -quality 92 $bigFile $postFileThumb" );
-
-    echo "Converting to small display versions at $postFile, $postFileThumb<br>";
-
-    chmod( $postFile, 0666 );
-    chmod( $postFileThumb, 0666 );
-
-    echo "Upload complete.<br><br>";
-    
-    echo "<br><br><a href=artLogUpload.php>Upload more</a><br><br>";
-    
-    echo "<a href=artLogPage.php>Go to art log</a>";
-    
+    rescanFiles();
     }
 
+
+
+?>
+
+
+<script type="text/javascript" src="sha1.js"></script>
+
+<script type="text/javascript">
+
+
+  function calcHMAC() {
+      try {
+  
+          var hmacInput = document.getElementById("hmacInputText");
+          var pepperInput = document.getElementById("pepperInputText");
+          var hmacOutput = document.getElementById("hmacOutputText");
+  
+          var shaObj = new jsSHA("SHA-1", "TEXT");
+          shaObj.setHMACKey( pepperInput.value, "TEXT" );
+          shaObj.update( hmacInput.value );
+          hmacOutput.value = shaObj.getHMAC("HEX");
+
+          } catch(e) {
+              hmacOutput.value = e.message
+          }
+      }
+</script>
+
+
+<?php
+
+$hashValueString = "";
+$passwordFill = "";
+
+if( isset( $_REQUEST[ "passwordHMAC" ] ) ) {
+    $passwordSubmittedHash = $_REQUEST[ "passwordHMAC" ];
+    
+    preg_match( "/[0-9a-f]+/", $passwordSubmittedHash, $matches );
+    $passwordSubmittedHash = $matches[0];
+
+    $hashValueString = "value='$passwordSubmittedHash'";
+    $passwordFill = "aaaaaaaaaa";
+    }
+
+?>
+
+
+<FORM>
+      Password:
+<INPUT TYPE="password" MAXLENGTH=20 SIZE=20 NAME="password" autofocus id="hmacInputText" onkeyup="calcHMAC()" value=<?php echo $passwordFill;?> >
+</FORM>
+
+
+
+<FORM enctype="multipart/form-data" ACTION="artLogUpload.php" METHOD="post">
+
+
+  Top image:<br>
+  <table border=1 cellpadding=5 cellspacing=0>
+	<tr>
+<?php
+
+      for( $i=0; $i<$numThumbs; $i++ ) {
+          $t = $thumbs[$i];
+
+          preg_match( "/([0-9]+)_t.jpg/", $t, $matches );
+
+          $num = $matches[1];
+          
+          echo "<td align=center valign=bottom><img src='artPosts/$t'><br>\n";
+          $checked = "";
+          if( $i == 0 ) {
+              $checked = "checked";
+              }
+          echo "<input type=radio name=newTop value=$num $checked>\n";
+          echo "</td>\n";
+
+          if( $i > 0 && $i % 6 == 0 ) {
+              echo "</tr><tr>";
+              }
+          }
+
+
+?>
+
+</tr></table>
+
+    <INPUT TYPE="hidden" NAME="upload" VALUE="1">
+
+<br>
+<br>
+Server-provided Pepper:
+<br>
+	<input type="text" size="75" name="pepper" readonly value="<?php echo $passwordHashingPepper;?>" id="pepperInputText">
+
+    <br>
+hmac_sha1 of password with pepper as key:<br>
+	<input type="text" size="75" name="passwordHMAC" 
+		   readonly id="hmacOutputText" <?php echo $hashValueString;?> >
+
+      <br><br><br><br>
+      Post this image file: <input name="userfile" type="file" />
+      <br>
+      <br>
+       <INPUT TYPE="Submit" VALUE="Update">
+
+      
+    </FORM>
+
+
+</body>
+
+
+<?php
+      
+    
 
 
 ?>
--- documentation/html/passwordHashUtility_artLog.php
+++ documentation/html/passwordHashUtility_artLog.php
@@ -8,6 +8,7 @@ if( isset( $_REQUEST[ "password" ] ) ) {
     $hash = hash_hmac( "sha1", 
                        $_REQUEST[ "password" ],
                        $passwordHashingPepper );
+    $hash = hash_hmac( "sha1", $hash, $passwordHashingPepper );
     
     echo "Password hash is:  $hash";
     }
