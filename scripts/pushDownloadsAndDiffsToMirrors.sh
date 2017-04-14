90e0e9ff022bcb29973917b760c4ea2d50dadd00
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-04-14 10:55:34 -0700

    Script to rsync downloads and diff bundles to remote servers.

---
+++ scripts/pushDownloadsAndDiffsToMirrors.sh
@@ -0,0 +1,15 @@
+while read user server
+do
+  echo ""
+  echo "Using rsync to sync all .dbz files to server to $server"
+  echo ""
+
+  rsync -avz -e ssh --progress ~/diffBundles/*.dbz $user@$server:downloads/
+
+  echo ""
+  echo "Using rsync to sync all oneLifeDownloads to server to $server"
+  echo ""
+
+  rsync -avz --exclude='*.ini' --exclude="*~" -e ssh --progress ~/oneLifeDownloads/* $user@$server:downloads/
+
+done <  <( grep "" ~/diffBundles/remoteServerList.ini )
