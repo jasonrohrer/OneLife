3cba96b66c0e541b37d600033a0d70504400170e
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-04-14 11:26:15 -0700

    Script for setting up backup server.

---
+++ scripts/linodeBackupServerRootSetup.sh
@@ -0,0 +1,47 @@
+# For remote backup server
+
+# Run as root to perform initial setup and user account creation
+
+# Runs interactively (asks for a password)
+
+apt-get -o Acquire::ForceIPv4=true update
+apt-get -y install emacs-nox
+
+
+echo ""
+echo ""
+echo "Setting up new user account jcr13 without a password..."
+echo ""
+echo ""
+
+useradd -m -s /bin/bash jcr13
+
+
+su jcr13<<EOSU
+
+cd /home/jcr13
+
+mkdir .ssh
+
+chmod 744 .ssh
+
+cd .ssh
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsiD85AwaNxcUzNzHPapVaGVIQCTUfdKT2tyd26MWqEds2UHLZund+S930BWz7guu3/mzuTomJnPxZPSyb+62ZiuAR0YaGZwYwMrFkrbPsXf6//MZDBvdMMcqPyqLj3Iny2ZZ9LTnSIs0hqQ3SksvP/qqHthS1YQWMwZlRxs6MmZdEuy4qZZgpnexf6uaWTEcxEO2Nij8LdEN8+jJZLHVkXSSD9c/ssTmdXss3/sVSZHYR+28HeqUahRsO0Rz6mR3FwB+ZslZlWiMFTzjkH5IA/XOiNK5Ezf1+EsQOn2OVfaCMlfJQ6YGp1kgFI01j2ZWHnqHaacvVc+C+9fAmIscZw==" > authorized_keys
+
+chmod 644 authorized_keys
+
+
+cd /home/jcr13
+
+mkdir backups
+
+exit
+EOSU
+
+
+echo ""
+echo "Done with remote setup."
+echo ""
+
+
