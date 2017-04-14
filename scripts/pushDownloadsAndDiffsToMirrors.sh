85c9539b6d69ac4ab6d4c5497c28d856b398f07e
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-04-14 12:11:29 -0700

    Fixed typo

--- scripts/pushDownloadsAndDiffsToMirrors.sh
+++ scripts/pushDownloadsAndDiffsToMirrors.sh
@@ -1,13 +1,13 @@
 while read user server
 do
   echo ""
-  echo "Using rsync to sync all .dbz files to server to $server"
+  echo "Using rsync to sync all .dbz files to $server"
   echo ""
 
   rsync -avz -e ssh --progress ~/diffBundles/*.dbz $user@$server:downloads/
 
   echo ""
-  echo "Using rsync to sync all oneLifeDownloads to server to $server"
+  echo "Using rsync to sync all oneLifeDownloads to $server"
   echo ""
 
   rsync -avz --exclude='*.ini' --exclude="*~" -e ssh --progress ~/oneLifeDownloads/* $user@$server:downloads/
