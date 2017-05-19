5d3ebcba3e3cd96173659596a576911bc25cab6f
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-05-19 16:58:36 -0700

    Noted SDL 1.2 in compile notes.

--- documentation/html/compileNotes.php
+++ documentation/html/compileNotes.php
@@ -4,7 +4,7 @@
 <table border=0 width="100%" cellpadding=5><tr><td bgcolor="#222222">
 Make sure you have the <b>dev</b> package of the following library installed:
 <ul>
-<li>libsdl
+<li>libsdl1.2
 </ul>
 The included "runToBuild" script should build the game for you automatically.
 
