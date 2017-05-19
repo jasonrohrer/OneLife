d6ca478ee52f0c2630790ef8fb1964e30c5aee7c
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-05-19 15:29:50 -0700

    Updated credits.

--- documentation/html/credits.php
+++ documentation/html/credits.php
@@ -1,9 +1,26 @@
 <?php include( "header.php" ); ?>
 
+All code, drawings, graphics, music, sounds, and fonts by Jason Rohrer, except for:
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+KISSDB server database by <a href="https://github.com/adamierymenko/kissdb">Adam Ierymenko</a>.<br><br>
+
+The font used on the menu screens is Futura by <a href="https://en.wikipedia.org/wiki/Futura_(typeface)">Paul Renner</a>.<br><br>
 
 FFT code by <a href="http://www.kurims.kyoto-u.ac.jp/~ooura/fft.html">Takuya Ooura</a>.<br><br>
 
 OGG Vorbis decoder by <a href="http://nothings.org/stb_vorbis/">Sean Barrett</a?.<br><br>
 
+Reverb impulse response data from <a href="http://www.echothief.com/woodruff-lane/">EchoThief</a>.<br><br>
 
+The <a href="http://www.libsdl.org/">SDL</a> library provides cross-platform screen, sound, and user input.<br><br>
+																			   
+<a href="http://www.mingw.org/">MinGW</a> was used to build the game for Windows.<br><br>
+    
 <?php include( "footer.php" ); ?>
