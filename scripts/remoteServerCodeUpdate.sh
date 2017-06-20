84aa8caac2678a66f348263a7b6e9bdc240e1b2f
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-06-20 11:01:17 -0700

    Added code for rebuilding binary code on all servers separate from data.

---
+++ scripts/remoteServerCodeUpdate.sh
@@ -0,0 +1,56 @@
+echo "" 
+echo "Shutting down server"
+echo ""
+
+serverPID=`pgrep OneLifeServer`
+
+if [ -z $serverPID ]
+then
+	echo "Server not running!"
+else
+	echo -n "1" > ~/checkout/OneLife/server/settings/shutdownMode.ini
+
+	echo "" 
+	echo "Set server shutdownMode, waiting for server to exit"
+	echo ""
+
+        while kill -CONT $serverPID 1>/dev/null 2>&1; do sleep 1; done
+
+	echo "" 
+	echo "Server has shutdown"
+	echo ""
+fi
+
+
+
+
+echo "" 
+echo "Re-compiling server"
+echo ""
+
+cd ~/checkout/minorGems
+hg pull
+hg update
+
+
+cd ~/checkout/OneLife/server
+hg pull
+hg update
+
+./configure 1
+make
+
+
+
+echo "" 
+echo "Re-launching server"
+echo ""
+
+
+echo -n "0" > ~/checkout/OneLife/server/settings/shutdownMode.ini
+
+
+
+cd ~/checkout/OneLife/server/
+
+sh ./runHeadlessServerLinux.sh
---
+++ scripts/updateServerCode.sh
@@ -0,0 +1,98 @@
+# script run on main server to shutdown all servers and update them
+# to the latest binary server code.  Does NOT update game data for the servers.
+
+
+# shut them all down
+
+
+echo "" 
+echo "Shutting down local server, set server shutdownMode flag."
+echo ""
+
+
+echo -n "1" > ~/checkout/OneLife/server/settings/shutdownMode.ini
+
+
+echo "" 
+echo "Shutting down remote servers, setting server shutdownMode flags."
+echo ""
+
+# feed file through grep to add newlines at the end of each line
+# otherwise, read skips the last line if it doesn't end with newline
+while read user server port
+do
+  echo "  Shutting down $server"
+  ssh $user@$server '~/checkout/OneLife/scripts/remoteServerShutdown.sh'
+done <  <( grep "" ~/www/reflector/remoteServerList.ini )
+
+
+
+serverPID=`pgrep OneLifeServer`
+
+if [ -z $serverPID ]
+then
+	echo "Local server not running!"
+else
+
+	echo "" 
+	echo "Waiting for local server to exit"
+	echo ""
+
+        while kill -CONT $serverPID 1>/dev/null 2>&1; do sleep 1; done
+
+	echo "" 
+	echo "Local server has shutdown"
+	echo ""
+fi
+
+
+
+
+echo "" 
+echo "Re-compiling local server"
+echo ""
+
+cd ~/checkout/minorGems
+hg pull
+hg update
+
+cd ~/checkout/OneLife/server
+hg pull
+hg update
+
+./configure 1
+make
+
+
+
+echo "" 
+echo "Re-launching local server"
+echo ""
+
+
+echo -n "0" > ~/checkout/OneLife/server/settings/shutdownMode.ini
+
+
+cd ~/checkout/OneLife/server/
+
+sh ./runHeadlessServerLinux.sh
+
+
+
+
+echo "" 
+echo "Triggering remote server rebuilds."
+echo ""
+
+
+# contiue with remote server rebuilds
+while read user server port
+do
+  echo "  Starting update on $server"
+  ssh $user@$server '~/checkout/OneLife/scripts/remoteServerCodeUpdate.sh'
+done <  <( grep "" ~/www/reflector/remoteServerList.ini )
+
+
+echo "" 
+echo "Done updating code on all servers."
+echo ""
