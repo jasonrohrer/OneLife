a1c0b18f9d6285580597c100b6266459ebaf2b02
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-10-03 11:04:02 -0700

    Moved trigger code out into separate file.  This allows for easy swap-out of different trigger configurations.

--- server/triggers.cpp
+++ server/triggers.cpp
@@ -41,38 +41,6 @@ SimpleVector<LiveDummySocket> dummySockets;
 
     
 
-void initTriggers() {
-    triggersEnabled = 
-        SettingsManager::getIntSetting( "allowTriggerRequests", 0 );
-    
-    if( triggersEnabled ) {
-        
-        if( SettingsManager::getIntSetting( "requireClientPassword", 1 )
-            ||
-            SettingsManager::getIntSetting( "requireTicketServerCheck", 1 ) ) {
-            
-            printf( "\n\nERROR:  Client-issued triggers enabled "
-                    "(allowTriggerRequests in settings folder), but "
-                    "requireTicketServerCheck or requireClientPassword "
-                    "is enabled in server settings folder.  Exiting.\n\n" );
-            exit( 0 );
-            }
-
-        ClothingSet clothing = getEmptyClothingSet();
-        
-        clothing.hat = getObject( 199 );
-        clothing.bottom = getObject( 200 );
-
-        PlayerMapping narrator = {
-            "test@test.com",
-            433,
-            20,
-            { 0, 0 },
-            0, clothing };
-        
-        playerMap.push_back( narrator );
-        }
-    }
 
 
 
@@ -282,7 +250,6 @@ void sendDummyAction( LiveDummySocket *inDummy,
 
 
 
-LiveDummySocket *alice = NULL;
 
 
 
@@ -308,74 +275,40 @@ static SimpleVector<GridPos> *finishMove() {
 
 
 
+// include file here that implements
+// customInit
+// and
+// customTrigger
+#include "triggers/test.cpp"
+
+
+
+void initTriggers() {
+    triggersEnabled = 
+        SettingsManager::getIntSetting( "allowTriggerRequests", 0 );
+    
+    if( triggersEnabled ) {
+        
+        if( SettingsManager::getIntSetting( "requireClientPassword", 1 )
+            ||
+            SettingsManager::getIntSetting( "requireTicketServerCheck", 1 ) ) {
+            
+            printf( "\n\nERROR:  Client-issued triggers enabled "
+                    "(allowTriggerRequests in settings folder), but "
+                    "requireTicketServerCheck or requireClientPassword "
+                    "is enabled in server settings folder.  Exiting.\n\n" );
+            exit( 0 );
+            }
+        
+        customInit();
+        }
+    }
+
+
 
 void trigger( int inTriggerNumber ) {
     
-    
-    GridPos offset = { 0, 0 };
-    ClothingSet clothing = getEmptyClothingSet();
-
-    
-    // increment this as we check for matches
-    // thus, we can rearrange trigger order below
-    // without having to update all the trigger numbers
-    int t = 0;
-    
-    
-    if( inTriggerNumber == t++ ) {
-        setMapObject( 2, 0, 418 );
-        }
-    else if( inTriggerNumber == t++ ) {
-        setMapObject( 2, 0, 153 );
-        }
-    else if( inTriggerNumber == t++ ) {
-        GridPos startPos = { 1, 1 };
-        
-        clothing.hat = getObject( 199 );
-        clothing.bottom = getObject( 200 );
-        
-        
-        LiveDummySocket s = newDummyPlayer( "dummy1@test.com", 350, 20,
-                                            startPos,
-                                            71,
-                                            clothing );
-        
-        dummySockets.push_back( s );
-
-        
-        alice = 
-            dummySockets.getElement( dummySockets.size() - 1 );        
-        }
-    else if( inTriggerNumber == t++ ) {
-        addToMove( 1, 0 );
-        addToMove( 2, 0 );
-        
-        sendDummyMove( alice, finishMove() );
-        }
-    else if( inTriggerNumber == t++ ) {
-        offset.y = 1;
-        sendDummyAction( alice, "DROP", offset, true, -1 );
-        }
-    else if( inTriggerNumber == t++ ) {
-        addToMove( -1, 0 );
-        addToMove( -2, 0 );
-        
-        sendDummyMove( alice, finishMove() );
-        }
-    else if( inTriggerNumber == t++ ) {
-        offset.y = 1;
-        sendDummyAction( alice, "USE", offset );
-        }
-    else if( inTriggerNumber == t++ ) {        
-        addToMove( -1, 0 );
-
-        sendDummyMove( alice, finishMove() );
-        }
-    else if( inTriggerNumber == t++ ) {
-        offset.y = 1;
-        sendDummyAction( alice, "USE", offset );
-        }
-    
+    customTrigger( inTriggerNumber );
     }
 
 
---
+++ server/triggers/test.cpp
@@ -0,0 +1,93 @@
+
+
+
+static void customInit() {
+    ClothingSet clothing = getEmptyClothingSet();
+    
+    clothing.hat = getObject( 199 );
+    clothing.bottom = getObject( 200 );
+    
+    PlayerMapping narrator = {
+        "test@test.com",
+        433,
+        20,
+        { 0, 0 },
+        0, clothing };
+    
+    playerMap.push_back( narrator );
+    }
+
+
+
+static LiveDummySocket *alice = NULL;
+
+
+
+static void customTrigger( int inTriggerNumber ) {
+    
+    GridPos offset = { 0, 0 };
+    ClothingSet clothing = getEmptyClothingSet();
+
+    
+    // increment this as we check for matches
+    // thus, we can rearrange trigger order below
+    // without having to update all the trigger numbers
+    int t = 0;
+    
+    
+    if( inTriggerNumber == t++ ) {
+        setMapObject( 2, 0, 418 );
+        }
+    else if( inTriggerNumber == t++ ) {
+        setMapObject( 2, 0, 153 );
+        }
+    else if( inTriggerNumber == t++ ) {
+        GridPos startPos = { 1, 1 };
+        
+        clothing.hat = getObject( 199 );
+        clothing.bottom = getObject( 200 );
+        
+        
+        LiveDummySocket s = newDummyPlayer( "dummy1@test.com", 350, 20,
+                                            startPos,
+                                            71,
+                                            clothing );
+        
+        dummySockets.push_back( s );
+
+        
+        alice = 
+            dummySockets.getElement( dummySockets.size() - 1 );        
+        }
+    else if( inTriggerNumber == t++ ) {
+        addToMove( 1, 0 );
+        addToMove( 2, 0 );
+        
+        sendDummyMove( alice, finishMove() );
+        }
+    else if( inTriggerNumber == t++ ) {
+        offset.y = 1;
+        sendDummyAction( alice, "DROP", offset, true, -1 );
+        }
+    else if( inTriggerNumber == t++ ) {
+        addToMove( -1, 0 );
+        addToMove( -2, 0 );
+        
+        sendDummyMove( alice, finishMove() );
+        }
+    else if( inTriggerNumber == t++ ) {
+        offset.y = 1;
+        sendDummyAction( alice, "USE", offset );
+        }
+    else if( inTriggerNumber == t++ ) {        
+        addToMove( -1, 0 );
+
+        sendDummyMove( alice, finishMove() );
+        }
+    else if( inTriggerNumber == t++ ) {
+        offset.y = 1;
+        sendDummyAction( alice, "USE", offset );
+        }
+    
+    }
+
