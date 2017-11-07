80111a71aed4341d3110b3141d44c08e40d96a78
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-11-07 13:31:44 -0800

    Compile notes now mention git instead of hg.

--- documentation/html/compileNotes.php
+++ documentation/html/compileNotes.php
@@ -2,10 +2,11 @@
 
 <font size=6>Compiling from source</font><br>
 <table border=0 width="100%" cellpadding=5><tr><td bgcolor="#222222">
-Make sure you have the following package installed:
+Make sure you have the following packages installed:
 <ul>
-  <li>mercurial (source control management, also called "hg")
+  <li>git (source control management)
   <li>imagemagick (image conversion tools)
+  <li>xclip (access to X Windows clipboard)
 </ul>
 Make sure you have the <b>dev</b> package of the following library installed:
 <ul>
