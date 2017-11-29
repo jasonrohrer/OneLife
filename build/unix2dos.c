670066b4b5b6b29144ba8d45a05246e5c1b2d0ea
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-11-29 10:27:56 -0800

    Fixed unix2dos conversion program behavior on Windows.

--- build/unix2dos.c
+++ build/unix2dos.c
@@ -11,8 +11,14 @@ main(void)
 			exit(0);
 
 		if(c == '\n') {
+#ifdef WIN32
+            /* putchar on windows auto-converts \n to \r\n */
+			putchar( '\n' );
+#else
+            /* other platforms must output CR LF manually */
 			putchar(015);	/* ^M */
 			putchar(012);	/* ^J */
+#endif
 		} else {
 			putchar(c);
 		}
