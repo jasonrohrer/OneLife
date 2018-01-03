aaefe0c5485a81d1535a74269ff9444b69c75954
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-01-03 15:12:09 -0800

    Added script for pulling and building test system for end users.

---
+++ scripts/pullAndBuildTestSystem.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+if [ ! -e minorGems ]
+then
+	git clone https://github.com/jasonrohrer/minorGems.git	
+fi
+
+if [ ! -e OneLife ]
+then
+	git clone https://github.com/jasonrohrer/OneLife.git
+fi
+
+if [ ! -e OneLifeData7 ]
+then
+	git clone https://github.com/jasonrohrer/OneLifeData7.git	
+fi
+
+
+
+cd minorGems
+git pull
+
+cd ../OneLife
+git pull
+
+cd ../OneLifeData7
+git pull
+
+rm */cache.fcz
+
+
+cd ../OneLife
+
+./configure 1
+
+cd gameSource
+
+make
+
+echo 1 > settings/useCustomServer.ini
+
+sh ./makeEditor.sh
+
+ln -s ../../OneLifeData7/animations .
+ln -s ../../OneLifeData7/categories .
+ln -s ../../OneLifeData7/ground .
+ln -s ../../OneLifeData7/music .
+ln -s ../../OneLifeData7/objects .
+ln -s ../../OneLifeData7/overlays .
+ln -s ../../OneLifeData7/scenes .
+ln -s ../../OneLifeData7/sounds .
+ln -s ../../OneLifeData7/sprites .
+ln -s ../../OneLifeData7/transitions .
+
+
+cd ../server
+./configure 1
+make
+
+
+ln -s ../../OneLifeData7/categories .
+ln -s ../../OneLifeData7/objects .
+ln -s ../../OneLifeData7/transitions .
+
+
+echo 0 > settings/requireTicketServerCheck.ini
