5606d0310cdd98777e46e384335ecfe0c0448283
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-01-09 13:04:30 -0800

    Added buy page and requirements page.

---
+++ documentation/html/buy.php
@@ -0,0 +1,133 @@
+<?php include( "header.php" ); ?>
+
+<?php include( "reviewPages/reviewCount.php" ); ?>
+
+
+<?php
+function showPayLinks( $inSimple ) {
+    $referrer = "";
+    
+    if( isset( $_SERVER['HTTP_REFERER'] ) ) {
+        // pass it through without a regex filter
+        // because we can't control its safety in the end anyway
+        // (user can just edit URL sent to FastSpring).
+        $referrer = urlencode( $_SERVER['HTTP_REFERER'] );
+        }
+    
+    
+
+    ?>
+ <center>
+      <center><table border=0><tr><td> 
+<ul> 
+      <li>Lifetime server account
+      <li>All future updates
+      <li>Full source code
+      <li>Tech support included
+      </ul>
+</td></tr></table>
+
+      <font size=5>Available now for $20</font><br>
+      <!--<font size=4 color=#ff0000>(25% off final price during launch week)</font><br><br>-->
+      
+      <a href="https://sites.fastspring.com/jasonrohrer/instant/onehouronelife?referrer=<?php echo $referrer;?>">
+      <img src="fs_cards.png" width=280 height=45 border=0><?php
+      if( !$inSimple ) {
+
+          echo "<br>";
+          
+          echo "<img src=\"fs_button05.png\" width=210 height=60 border=0>";
+          }
+?>
+      </a>
+      </center>
+<?php
+    }
+
+function showLogo( $inImageFile, $inText ) {
+
+    echo "<table border=0><tr><td align=center>
+          <img src=\"$inImageFile\"><br>
+          <font size=1>$inText</font>
+          </td></tr></table>";
+    }
+
+?>
+
+
+
+<center>
+
+
+<table border=0 width=100%>
+<tr>
+<td><img src=portraitLeft.png border=0 width=197 height=414></td>
+<td>
+<center><?php include( "lifeStats.php" ); ?></center>
+
+
+
+<img src=logo558x305_border.png border=0 width=558 height=305>
+
+<br>
+<center>a multiplayer survival game of parenting<br>and civilization building by <a href=http://hcsoftware.sf.net/jason-rohrer>Jason Rohrer</a></center>
+
+</td>
+<td><img src=portraitRight.png border=0 width=196 height=418></td>
+
+</tr>
+</table>
+
+
+ <center>
+<table border=0><tr>
+<td><?php showLogo( "noDRM.png", "No DRM" ); ?></td>
+<td><?php showLogo( "noTie.png", "No middle-person" ); ?></td>
+<td><?php showLogo( "crossPlatform.png", "Cross-platform" ); ?></td>
+<td><?php showLogo( "openSource.png", "Open Source" ); ?></td>
+</tr></table>                                  
+</center>
+
+<center>
+<table border=0 cellpadding=2><tr><td bgcolor="#222222">
+<table border=0 cellpadding=5><tr><td bgcolor="#000000">
+<center> 
+
+<?php
+   showPayLinks( false );
+?>
+
+</td></tr></table>
+</td></tr></table>
+</center>
+<br>
+<br>
+</center>
+
+<font size=5>What you get</font><br>
+
+<table border=0 width="100%" cellpadding=5><tr><td bgcolor="#222222">
+Immediately after your payment is processed, you will receive an email with an access link.  You will then be able to download all of the following DRM-free distributions:
+<center>
+<table border=0><tr><td>
+<ul>
+<li>Windows build</li>
+<li>MacOS build (10.5 and later Intel)</li>
+<li>GNU/Linux build (compiled on 32-bit Ubuntu 14.04)</li>
+<li>Full source code bundle (compile it yourself)</li>
+</ul>
+</td></tr></table>
+</center>
+The price also includes downloads of all future updates and a lifetime account on the main game server that I am running.<br>
+<br>
+The source bundle includes the editor and server software, allowing you to <b>set up and run your own server</b>.<br>
+<br>
+You can take a look at the <a href="requirements.php">system requirements</a>.
+</td></tr></table>
+<br>
+<br>
+
+
+     
+    
+<?php include( "footer.php" ); ?>
---
+++ documentation/html/requirements.php
@@ -0,0 +1,22 @@
+<?php
+
+
+include( "header.php" ); ?>
+
+<font size=6>System Requirements</font><br>
+<table border=0 width="100%" cellpadding=5><tr><td bgcolor="#222222">
+The game was developed on an old laptop from 2010 with an <a href="http://www.nvidia.com/object/nvs_techspecs.html">NVS 3100M GPU</a> where it runs at 60 frames per second and is perfectly playable.  In other words, it will most likely run on your computer.<br>
+<br>
+    The only required library is OpenGL, which ships standard on MacOS and Windows and is readily available on GNU/Linux as well.  On GNU/Linux, you will also need to install SDL 1.2 and xclip for copy/paste support.<br>
+<br>
+Other stuff you will need:
+<ul>
+<li>keyboard</li>
+<li>mouse</li>
+<li>speakers or headphones</li>
+</ul>
+</td></tr></table>
+<br>
+<br>
+
+<?php include( "footer.php" ); ?>
