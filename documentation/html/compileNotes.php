5bb17ac0d37644372f18a1183cd486379020df72
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-03-07 08:00:56 -0800

    Improved the compile notes with more details.

--- documentation/html/compileNotes.php
+++ documentation/html/compileNotes.php
@@ -5,14 +5,41 @@
 Make sure you have the following packages installed:
 <ul>
   <li>git (source control management)
+  <li>g++ (compiler)
   <li>imagemagick (image conversion tools)
   <li>xclip (access to X Windows clipboard)
 </ul>
-Make sure you have the <b>dev</b> package of the following library installed:
+Make sure you have the <b>dev</b> package of the following libraries installed:
 <ul>
 <li>libsdl1.2
+<li>libgl     
+<li>libglu     
 </ul>
-The included "runToBuild" script should build the game for you automatically.
+On Ubuntu, for example, the package names are:
+<ul>
+<li>git
+<li>g++
+<li>imagemagick
+<li>xclip
+<li>libsdl1.2-dev  
+<li>libglu1-mesa-dev  
+<li>libgl1-mesa-dev
+</ul>
+If you want to do this from the command line, you would run:
+<pre><code>    sudo apt-get install git g++ imagemagick xclip libsdl1.2-dev libglu1-mesa-dev libgl1-mesa-dev</code></pre>
+
+<br>
+<br>
+After installing the required packages on your system, download the UnixSource bundle and extract it:
+
+<pre><code>    tar xzf OneLife_Live4_UnixSource.tar.gz</code></pre>
+
+The included "pullAndBuildLatest" script should pull from git and build the game for you automatically.  You would run it like this:
+
+<pre><code>    cd OneLife_Live4_UnixSource
+    ./pullAndBuildLatest
+</code></pre>
+
 
 </td></tr></table>
 <br>
