0cbf800765769e731088f6ac4e0027e977e8582c
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-03-12 22:37:01 -0700

    Better messages around remote server startup.

--- scripts/remoteServerStartup.sh
+++ scripts/remoteServerStartup.sh
@@ -2,13 +2,15 @@ echo ""
 echo "Re-launching server"
 echo ""
 
-
+echo "    Setting shutdownMode to 0"
 echo -n "0" > ~/checkout/OneLife/server/settings/shutdownMode.ini
 
 
 
 cd ~/checkout/OneLife/server/
 
+echo "    Running runHeadlessServerLinux"
 sh ./runHeadlessServerLinux.sh
 
+echo "    Setting keepServerRunning flag"
 echo -n "1" > ~/keepServerRunning.txt
