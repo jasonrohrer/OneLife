939b663ce6e479ebae719e5a625e28876deed020
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-03-12 22:46:08 -0700

    Fixed so runHeadless script won't hang ssh.

--- server/runHeadlessServerLinux.sh
+++ server/runHeadlessServerLinux.sh
@@ -1,2 +1,2 @@
 ./makeLogRotator
-nohup catchsegv unbuffer ./OneLifeServer | ./logRotator 20000000 serverOut.txt &
+nohup catchsegv unbuffer ./OneLifeServer | ./logRotator 20000000 serverOut.txt >/dev/null &
