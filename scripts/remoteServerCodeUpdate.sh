082c4e57cdaea3fed316ec29039183d069eb9daf
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-04-07 16:10:56 -0700

    Fixed path.

--- scripts/remoteServerCodeUpdate.sh
+++ scripts/remoteServerCodeUpdate.sh
@@ -44,6 +44,6 @@ make
 git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags | sed -e 's/OneLife_v//' > serverCodeVersionNumber.txt
 
 
-./remoteServerCodeUpdateCustomPost.sh
+~/checkout/OneLife/scripts/remoteServerCodeUpdateCustomPost.sh
 
 
