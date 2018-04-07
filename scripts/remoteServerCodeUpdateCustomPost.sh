10b2515cf777da4af0c8cf8c64e69b5a7df78357
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-04-07 16:09:24 -0700

    Added a one-time custom post step for server code update (to clear look time and biome db).

---
+++ scripts/remoteServerCodeUpdateCustomPost.sh
@@ -0,0 +1,9 @@
+# custom steps that are executed after a server code update
+
+# stuff we want to do only one time, like clear certain database
+
+cd ~/checkout/OneLife/server
+
+rm lookTime.db
+rm biome.db
+
