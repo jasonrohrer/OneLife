3d7dbf24cad93f599c1127ce6c78eadcc09f4646
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-04-07 16:57:55 +0000

    Added a BUY link to header.

--- documentation/html/header.php
+++ documentation/html/header.php
@@ -7,6 +7,16 @@
 global $pathToRoot;
 
 
+$referrer = "";
+    
+if( isset( $_SERVER['HTTP_REFERER'] ) ) {
+    // pass it through without a regex filter
+    // because we can't control its safety in the end anyway
+    // (user can just edit URL sent to FastSpring).
+    $referrer = urlencode( $_SERVER['HTTP_REFERER'] );
+    }
+    
+
 if( isset( $blockRobots ) && $blockRobots == 1 ) {
 ?>
     <meta name="robots" content="noindex, nofollow">
@@ -23,15 +33,16 @@ if( isset( $blockRobots ) && $blockRobots == 1 ) {
     
     <table border=0 cellspacing=5 cellpadding=0 width=100%><tr>
 
-    <td align=center width=11%>[<a href='http://onehouronelife.com'>Home</a>]</td>
-    <td align=center width=11%>[<a href='http://onehouronelife.com/newsPage.php'>News</a>]</td>
-    <td align=center width=11%>[<a href='http://onehouronelife.com/updateLog.php'>Update Log</a>]</td>
-    <td align=center width=11%>[<a href='http://onehouronelife.com/foodStats.php'>Food Stats</a>]</td>
-    <td align=center width=11%>[<a href='http://onehouronelife.com/failureStats.php'>Fail Stats</a>]</td>
-    <td align=center width=11%>[<a href='https://onehouronelife.com/forums'>Forums</a>]</td>
-    <td align=center width=11%>[<a href='http://onehouronelife.com/artLogPage.php'>Artwork</a>]</td>
-    <td align=center width=11%>[<a href="http://onehouronelife.gamepedia.com">Wiki</a>]</td>
-    <td align=center width=11%>[<a href='http://onehouronelife.com/credits.php'>Credits</a>]</td>
+    <td align=center width=10%>[<a href='http://onehouronelife.com'>Home</a>]</td>
+    <td align=center width=10%>[<a href='https://sites.fastspring.com/jasonrohrer/instant/onehouronelife?referrer=<?php echo $referrer;?>'>Buy</a>]</td>
+    <td align=center width=10%>[<a href='http://onehouronelife.com/newsPage.php'>News</a>]</td>
+    <td align=center width=10%>[<a href='http://onehouronelife.com/updateLog.php'>Update Log</a>]</td>
+    <td align=center width=10%>[<a href='http://onehouronelife.com/foodStats.php'>Food Stats</a>]</td>
+    <td align=center width=10%>[<a href='http://onehouronelife.com/failureStats.php'>Fail Stats</a>]</td>
+    <td align=center width=10%>[<a href='https://onehouronelife.com/forums'>Forums</a>]</td>
+    <td align=center width=10%>[<a href='http://onehouronelife.com/artLogPage.php'>Artwork</a>]</td>
+    <td align=center width=10%>[<a href="http://onehouronelife.gamepedia.com">Wiki</a>]</td>
+    <td align=center width=10%>[<a href='http://onehouronelife.com/credits.php'>Credits</a>]</td>
 
 
 </tr></table>
