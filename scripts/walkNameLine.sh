bebf80dce6ca9c2eb73abb26eb4bd0d40c6f0edb
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-04-13 02:09:09 +0000

    Got name walking script working.

--- scripts/walkNameLine.sh
+++ scripts/walkNameLine.sh
@@ -5,23 +5,26 @@ if [ "$#" -lt "1" ] ; then
    exit;
 fi
 
+origID=$1
 id=$1
 
 
-echo -n "" > $id_line.txt
+echo -n "" > ${id}_line.txt
 
 
 while [[ ! -z "$id" ]]; do
-	name=`grep "67748" *name* | awk 'END {print $(NF-1), $NF}'`
-
-	echo -e "$name\n$(cat $id_line.txt)" > $id_line.txt
+	name=`grep "$id" *name* | awk 'END {print $(NF-1), $NF}' | tr A-Z a-z | awk '{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1'`
+	echo "Name = $name"
+	echo -e "$name\n$(cat ${origID}_line.txt)" > ${origID}_line.txt
 
 	noParentSearch=`grep "B [0-9]* $id .*noParent" *`;
-	
-	if [[ -z $noParentSearch ]]; then
+
+	#echo "NO parent search = '$noParentSearch'"
+	if [[ -n "$noParentSearch" ]]; then
 		id=""
+		#echo "No parent"
 	else
-		id=`grep "B [0-9]* 67748 .*parent" *  | awk -F"=" '{print $2}' | awk -F"," '{print $1}'`
+		id=`grep "B [0-9]* $id .*parent" *  | awk -F"=" '{print $2}' | awk -F"," '{print $1}'`
 	fi
 done
 
