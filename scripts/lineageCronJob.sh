ecd192da383a96a5b30002b9709505eab5ca7d9b
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-05-11 02:28:39 +0000

    Fixed header copy-over.

--- scripts/lineageCronJob.sh
+++ scripts/lineageCronJob.sh
@@ -5,9 +5,10 @@ cd documentation/html
 
 cp noBotsHeader.php ~/public_html
 cp noCounterFooter.php ~/public_html
-
+cp header.php ~/public_html
+cp footer.php ~/public_html
 
 cd ~/checkout/OneLifeData7
 git pull
 
-rsync faces/*.png ~public_html/faces/
+rsync faces/*.png ~/public_html/faces/
