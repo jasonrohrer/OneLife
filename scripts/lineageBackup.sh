73bf82ce9fc2724f8faf69411d45d303a671b783
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-05-17 10:33:06 -0700

    Lineage backup script now deletes files older than 2 weeks.

--- scripts/lineageBackup.sh
+++ scripts/lineageBackup.sh
@@ -5,3 +5,6 @@ date=`date +"%Y_%b_%d_%a"`
 
 mysqldump -u jcr13_lineageU --password=$passwordLineage jcr13_lineage > ~/backups/lineage_$date.mysql
 gzip -f ~/backups/lineage_$date.mysql
+
+# delete backup files older than two weeks
+find ~/backups -mtime +14 -delete
