f6c32d1b24d075a871f2a9e2e696b55b6ee748e2
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-07-10 16:16:33 -0700

    Data-only diff bundle generation script now skips scenes.  They never should have been included, and the tutorial map scene was BIG (26,000 lines).

--- scripts/generateDataOnlyDiffBundle.sh
+++ scripts/generateDataOnlyDiffBundle.sh
@@ -133,6 +133,7 @@ rm -rf ~/checkout/diffWorking/dataLatest/.git*
 rm ~/checkout/diffWorking/dataLatest/.hg*
 rm -rf ~/checkout/diffWorking/dataLatest/soundsRaw
 rm -rf ~/checkout/diffWorking/dataLatest/faces
+rm -rf ~/checkout/diffWorking/dataLatest/scenes
 rm -r ~/checkout/diffWorking/dataLatest/*.sh ~/checkout/diffWorking/dataLatest/working ~/checkout/diffWorking/dataLatest/overlays
 echo -n "$newVersion" > ~/checkout/diffWorking/dataLatest/dataVersionNumber.txt
 
@@ -148,6 +149,7 @@ rm -rf ~/checkout/diffWorking/dataLast/.git*
 rm ~/checkout/diffWorking/dataLast/.hg*
 rm -rf ~/checkout/diffWorking/dataLast/soundsRaw
 rm -rf ~/checkout/diffWorking/dataLast/faces
+rm -rf ~/checkout/diffWorking/dataLatest/scenes
 rm -r ~/checkout/diffWorking/dataLast/*.sh ~/checkout/diffWorking/dataLast/working ~/checkout/diffWorking/dataLast/overlays
 
 
