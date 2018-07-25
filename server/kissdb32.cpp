fd0071ca3e20e8b37d31acd20166f79defb8a9b0
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-07-24 18:09:55 -0700

    Switched to murmurhash in 32-bit version of KISSDB for testing.

--- server/kissdb32.cpp
+++ server/kissdb32.cpp
@@ -36,7 +36,7 @@
 #define KISSDB_HEADER_SIZE ((sizeof(uint64_t) * 3) + 4)
 
 /* djb2 hash function */
-static uint32_t KISSDB_hash(const void *b,unsigned long len)
+static uint32_t KISSDB_hash_old(const void *b,unsigned long len)
 {
 	unsigned long i;
 	uint32_t hash = 5381;
@@ -44,6 +44,69 @@ static uint32_t KISSDB_hash(const void *b,unsigned long len)
 		hash = ((hash << 5) + hash) + (uint32_t)(((const uint8_t *)b)[i]);
 	return hash;
 }
+
+
+
+//static uint32_t MurmurHash2 ( const void * key, int len, uint32_t seed )
+static uint32_t KISSDB_hash( const void * key, int len )
+{
+  // 'm' and 'r' are mixing constants generated offline.
+  // They're not really 'magic', they just happen to work well.
+
+  const uint32_t m = 0x5bd1e995;
+  const int r = 24;
+  
+  // usually passed in as a param
+  // fixed for use in KISSDB
+  const uint32_t seed = 34892093;
+  
+
+  // Initialize the hash to a 'random' value
+
+  uint32_t h = seed ^ len;
+
+  // Mix 4 bytes at a time into the hash
+
+  const unsigned char * data = (const unsigned char *)key;
+
+  while(len >= 4)
+  {
+    uint32_t k = *(uint32_t*)data;
+
+    k *= m;
+    k ^= k >> r;
+    k *= m;
+
+    h *= m;
+    h ^= k;
+
+    data += 4;
+    len -= 4;
+  }
+
+  // Handle the last few bytes of the input array
+
+  switch(len)
+  {
+  case 3: h ^= data[2] << 16;
+  case 2: h ^= data[1] << 8;
+  case 1: h ^= data[0];
+      h *= m;
+  };
+
+  // Do a few final mixes of the hash to ensure the last few
+  // bytes are well-incorporated.
+
+  h ^= h >> 13;
+  h *= m;
+  h ^= h >> 15;
+
+  return h;
+} 
+
+
+
+
 
 int KISSDB_open(
 	KISSDB *db,
