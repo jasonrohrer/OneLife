edc2160ff8db77e85a57aa38a923dca4fec60ac3
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-07-27 15:36:21 -0700

    Conversion code and script from stackdb to lineardb.

---
+++ server/convertDBs2.sh
@@ -0,0 +1,7 @@
+./dbConvert2 biome.db 80000 8 12 10000
+./dbConvert2 eve.db 5000 50 12 5000
+./dbConvert2 floor.db 80000 8 4 1000000
+./dbConvert2 floorTime.db 80000 8 8 5000
+./dbConvert2 lookTime.db 80000 8 8 1000000
+./dbConvert2 mapTime.db 80000 16 8 1000000
+./dbConvert2 map.db 80000 16 4 1000000
---
+++ server/dbConvert2.cpp
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+
+
+#include "minorGems/util/stringUtils.h"
+
+#include "kissdb.h"
+#include "stackdb.h"
+#include "lineardb.h"
+
+
+void usage() {
+    printf( "Usage:\n" );
+    printf( "dbConvert stack_db_file old_table_size key_size value_size new_table_size\n\n" );
+    
+    printf( "Example:\n" );
+    printf( "dbConvert map.db 80000 16 4 1000000\n\n" );
+    
+    exit( 1 );
+    }
+
+
+
+int main( int inNumArgs, char **inArgs ) {
+    
+    if( inNumArgs != 6 ) {
+        usage();
+        }
+
+    int oldTableSize = 0;
+    int keySize = 0;
+    int valueSize = 0;
+    int newTableSize = 0;
+
+
+    sscanf( inArgs[2], "%d", &oldTableSize );
+    
+    if( oldTableSize == 0 ) {
+        usage();
+        }
+
+    sscanf( inArgs[3], "%d", &keySize );
+    
+    if( keySize == 0 ) {
+        usage();
+        }
+
+
+    sscanf( inArgs[4], "%d", &valueSize );
+    
+    if( valueSize == 0 ) {
+        usage();
+        }
+
+
+    sscanf( inArgs[5], "%d", &newTableSize );
+    
+    if( newTableSize == 0 ) {
+        usage();
+        }
+
+
+    char *oldFileName = inArgs[1];
+    
+    STACKDB db;
+
+    int error = STACKDB_open( &db,
+                              oldFileName,
+                              0,
+                              oldTableSize,
+                              keySize,
+                              valueSize );
+    if( error ) {
+        printf( "dbConvert Failed to open stackdb file %s\n",
+                oldFileName );
+        exit( 1 );
+        }
+    
+    
+
+
+    char tempFileName[1000];
+    
+    sprintf( tempFileName, "%s.temp", oldFileName );
+    
+
+    LINEARDB dbNew;
+
+    error = LINEARDB_open( &dbNew,
+                          tempFileName,
+                          0,
+                          newTableSize,
+                          keySize,
+                          valueSize );
+    if( error ) {
+        printf( "dbConvert: Failed to open temp linear file %s\n", 
+                tempFileName );
+        STACKDB_close( &db );
+        exit( 1 );
+        }
+
+    // insert all keys from old to new
+    unsigned char *keyBuff = new unsigned char[ keySize ];
+    unsigned char *valueBuff = new unsigned char[ valueSize ];
+
+    STACKDB_Iterator dbi;
+    
+    STACKDB_Iterator_init( &db, &dbi );
+    
+    while( STACKDB_Iterator_next( &dbi, keyBuff, valueBuff ) > 0 ) {
+        LINEARDB_put( &dbNew, keyBuff, valueBuff );
+        }
+
+    delete [] keyBuff;
+    delete [] valueBuff;
+    
+    STACKDB_close( &db );
+    LINEARDB_close( &dbNew );
+        
+    if( rename ( tempFileName, oldFileName ) != 0 ) {
+        printf( "dbConvert: Failed to move temp file %s to "
+                "overwrite old stackdb file %s\n",
+                tempFileName, oldFileName );
+        }
+    
+
+    
+    return 0;
+    }
