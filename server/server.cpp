d1ca4132af04a4922088d4e359d88951f80c2ca3
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-07-30 12:40:12 -0700

    Disabled nightly server map backup operation.  Map databases have gotten so big that this operation, which must interrupt server activity for safety, now takes 10 seconds on server1.  These map backups have never been needed and take up substantial disk space.

--- server/server.cpp
+++ server/server.cpp
@@ -5267,7 +5267,7 @@ int main() {
     
 
     initLifeLog();
-    initBackup();
+    //initBackup();
     
     initPlayerStats();
     initLineageLog();
@@ -5372,7 +5372,7 @@ int main() {
         apocalypseStep();
         monumentStep();
         
-        checkBackup();
+        //checkBackup();
 
         stepFoodLog();
         stepFailureLog();
