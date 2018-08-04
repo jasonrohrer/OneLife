01afee343392d3158f23db53919088cf2b67bf65
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-08-03 20:32:45 -0700

    Convert script from stackdb to lineardb3.

---
+++ server/convertDBs3.sh
@@ -0,0 +1,7 @@
+./dbConvert3 biome.db 80000 8 12
+./dbConvert3 eve.db 5000 50 12
+./dbConvert3 floor.db 80000 8 4
+./dbConvert3 floorTime.db 80000 8 8
+./dbConvert3 lookTime.db 80000 8 8
+./dbConvert3 mapTime.db 80000 16 8
+./dbConvert3 map.db 80000 16 4
