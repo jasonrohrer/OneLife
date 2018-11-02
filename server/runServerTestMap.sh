9d73f94e017e8ff1ba9db31c097c0ea52c783c1b
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-11-02 13:32:35 -0700

    Delete map dummy recall list when running from fresh test map.

--- server/runServerTestMap.sh
+++ server/runServerTestMap.sh
@@ -1,6 +1,6 @@
 if [ -f ../gameSource/testMap.txt ]
 then
-	rm *.db testMapStale.txt; cp ../gameSource/testMap.txt .; make; ./OneLifeServer
+	rm *.db mapDummyRecall.txt testMapStale.txt; cp ../gameSource/testMap.txt .; make; ./OneLifeServer
 else
 	echo "testMap.txt does not exist in ../gameSource"
 fi
