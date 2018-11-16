7a5acd8bf2827563904da8ef94dc47479253ac7b
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-11-16 13:03:34 -0800

    Cached PHP files are kept in tmp.  Cache comment at top contains more info.

--- documentation/html/cacheTop.php
+++ documentation/html/cacheTop.php
@@ -7,16 +7,19 @@
 $url = $_SERVER[ "SCRIPT_NAME" ];
 $break = explode( '/', $url );
 $file = $break[ count( $break ) - 1 ];
-$cachefile = 'cached-' . substr_replace( $file , "", -4 ) . '.html';
+$cachefile = '/tmp/cached-' . substr_replace( $file , "", -4 ) . '.html';
 $cachetime = 120;
 
 // Serve from the cache if it is younger than $cachetime
 if( file_exists( $cachefile ) &&
     time() - $cachetime < filemtime( $cachefile ) ) {
 
+    $fileTime = filemtime( $cachefile );
+    $timeLeft = ( time() - $fileTime ) - $cachetime;
+    
     echo "<!-- Cached copy, generated " .
-        date( 'H:i', filemtime( $cachefile ) ) .
-        " -->\n";
+        date( 'H:i:s', filemtime( $cachefile ) ) .
+        ", $timeLeft sec remain until regen -->\n";
 
     include( $cachefile );
     exit;
