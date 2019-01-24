863318eb8be2f2e6739b71013f92049c04d8da5a
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-01-23 20:20:15 -0800

    Added loop to monitor over 30 minute test period.

--- server/monitorServer.sh
+++ server/monitorServer.sh
@@ -2,17 +2,20 @@ serverPID=`pidof OneLifeServer`
 
 echo "Server PID = $serverPID"
 
+# 360 is 30 minutes in 5-second steps 
+i=1
+while [ $i -lt 360 ]
+do 
 
-numPlayers=`nc -w 1 localhost 8005 | head -2 | tail -1 | sed 's/\/.*//'`
+	numPlayers=`nc -w 1 localhost 8005 | head -2 | tail -1 | sed 's/\/.*//'`
 
-echo "Num players = $numPlayers"
+	cpu=`top -b -n 1 -p $serverPID | grep $serverPID | awk '{print \$9}'`
 
+	cpuPerPlayer=`bc <<< "scale=2; $cpu / $numPlayers"`
 
-cpu=`top -b -n 1 -p $serverPID | grep $serverPID | awk '{print \$9}'`
+	echo "$i $numPlayers $cpu $cpuPerPlayer"
 
-echo "CPU Usage = $cpu"
+	sleep 5
 
-cpuPerPlayer=`bc <<< "scale=2; $cpu / $numPlayers"`
-
-
-echo "CPU per player $cpuPerPlayer"
+	i=$((i+1))
+done
