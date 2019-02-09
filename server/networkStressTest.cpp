9a7042fb344b69ce1e14ae8df6dafac1efc82c76
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-02-08 20:24:43 -0800

    Split test into smaller batches, 8192 bytes per second.

--- server/networkStressTest.cpp
+++ server/networkStressTest.cpp
@@ -4,7 +4,7 @@
 #include "minorGems/network/SocketServer.h"
 #include "minorGems/system/Thread.h"
 
-#define buffSize 4096
+#define buffSize 8192
 
 int main() {
     printf( "Listening on port 9000\n" );
@@ -24,23 +24,35 @@ int main() {
         
         unsigned char buffer[buffSize];
         
+        int totalSleep = 500;
+        
         while( ! fail ) {
             
-            const char *message = "This is a test message";
-            int len = strlen( message );
-            int numSent = 
-                sock->send( buffer, 
-                            buffSize, 
-                            false, false );
+            int splitCount = 64;
+            int len = buffSize / splitCount;
             
-            if( numSent != buffSize ) {
-                fail = true;
-                }
-            else {
-                totalSent += numSent;
-                }
+            int splitSleep = totalSleep / splitCount;
             
-            Thread::staticSleep( 1 );
+            for( int i=0; i<splitCount; i++ ) {
+                
+                int numSent = 
+                    sock->send( &( buffer[ i * len ] ), 
+                                len, 
+                                false, false );
+                
+                if( numSent != len ) {
+                    fail = true;
+                    }
+                else {
+                    totalSent += numSent;
+                    }
+                Thread::staticSleep( splitSleep );
+                }
+            // sleep after every full buffer
+            //Thread::staticSleep( 1000 );
+            if( totalSent % 131072 == 0 ) {
+                printf( "Sent %d\n", totalSent );
+                }
             }
         printf( "Total sent before fail:  %d\n", totalSent );
         
