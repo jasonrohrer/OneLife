d93744ebf1ab48eaef91f38c010b84226c49acc8
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-02-20 15:46:48 -0800

    Fixed glitch in counting adults.

--- server/getAveLife.sh
+++ server/getAveLife.sh
@@ -7,7 +7,7 @@ for f in *day.txt; do
 	then
 
 		date=`grep "D " $f | head -n 1 | sed -e 's/D //' | sed -e 's/ .*//'`
-		av=`grep "D " $f | sed -e "s/.*age=//" | sed -e "s/ .*//" | awk '$1 < 14.0' | awk '{s+=$1}END{print "",s/NR}' RS="\n"`
+		av=`grep "D " $f | sed -e "s/.*age=//" | sed -e "s/ .*//" | awk '$1 < 65.0' | awk '{s+=$1}END{print "",s/NR}' RS="\n"`
 		echo $f $date $num $av
 	fi
 done
