9abf7f1cfb2d6cbdafaec5991dd137ca00470421
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-08-02 13:18:42 -0700

    mapChangeLog cron script now avoids publishing most recent file (file for current, in-progress arc).

--- scripts/syncMapChangeLogs.sh
+++ scripts/syncMapChangeLogs.sh
@@ -4,4 +4,7 @@
 rsync -avz -e ssh --progress bigserver2.onehouronelife.com:checkout/OneLife/server/mapChangeLogs/*.txt ~/mapChangeLogs/
 
 # copy from here to public data server
-rsync -avz -e ssh --progress ~/mapChangeLogs/ publicdata.onehouronelife.com:public_html/publicMapChangeData/bigserver2.onehouronelife.com/
+# send all but latest file, which is not public knowledge yet (still being
+# updated with new data, seed still a secret)
+cd ~/mapChangeLogs/
+rsync -avz -e ssh --progress `ls -1tr | head -n -1` publicdata.onehouronelife.com:public_html/publicMapChangeData/bigserver2.onehouronelife.com/
