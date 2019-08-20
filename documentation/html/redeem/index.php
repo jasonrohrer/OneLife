1033a4b5972d2ff5adb268e2be2402c20fdaf7a5
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-08-20 10:23:15 -0700

    Added redeem page for coupon codes.

---
+++ documentation/html/redeem/index.php
@@ -0,0 +1,25 @@
+<?php include( "../header.php" ); ?>
+
+
+<center>
+<img src=../logo558x305_border.jpg border=0 width=558 height=305>
+
+<br>
+<br>
+<font size=5>Redeem a Code:</font>     
+
+<br>
+<br>
+
+
+<form action="https://sites.fastspring.com/jasonrohrer/instant/onehouronelife" 
+      method="post">
+Code: <input type="text" name="coupon" value="" width=20>
+<input type="submit" value="Redeem"><br>
+</form>
+
+<br>
+<br>
+
+
+<?php include( "../footer.php" ); ?>
