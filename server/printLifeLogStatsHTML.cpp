d9653559a11b61a704c91617425f27cae8e194e5
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-09-26 15:54:17 -0700

    Fixed handling of case where there are millions but 0 thousands in printCommaInt.  Fixes #405

--- server/printLifeLogStatsHTML.cpp
+++ server/printLifeLogStatsHTML.cpp
@@ -398,7 +398,7 @@ void printCommaInt( FILE *inFile, int inInt ) {
     if( billions > 0 ) {
         fprintf( inFile, "%d,", billions );
         }
-    if( millions > 0 ) {
+    if( billions > 0 || millions > 0 ) {
         if( origInt > 999999999 ) {
             fprintf( inFile, "%03d,", millions );
             }
@@ -406,7 +406,7 @@ void printCommaInt( FILE *inFile, int inInt ) {
             fprintf( inFile, "%d,", millions );
             }
         }
-    if( thousands > 0 ) {
+    if( billions > 0 || millions > 0 || thousands > 0 ) {
         if( origInt > 999999 ) {
             fprintf( inFile, "%03d,", thousands );
             }
