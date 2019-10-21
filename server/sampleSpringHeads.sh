32b17b21083289ae21c09d0910de03783cd7f6dc
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-10-21 09:44:02 -0700

    Printing names of each object sampled.

--- server/sampleSpringHeads.sh
+++ server/sampleSpringHeads.sh
@@ -2,6 +2,14 @@
 
 for ((y=-320;y<=320;y+=40)); do
 	for ((x=-320;x<=320;x+=40)); do
-		./dumpMapCoord $x $y
+		id=`./dumpMapCoord $x $y`
+		
+		if [[ $id == 0 ]];
+		then
+			name="Empty Spot"
+		else
+			name=$(cat objects/$id.txt | sed -n 2p );
+		fi
+		echo $name
 	done
 done
