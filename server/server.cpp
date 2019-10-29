2af074e203d6ab23743e1daee8a3ec48c1651c7d
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-10-29 14:40:18 -0700

    Holding a baby now cancels any kill target actions against it.  Fixes #421

--- server/server.cpp
+++ server/server.cpp
@@ -17885,8 +17885,10 @@ int main() {
             
             if( killer == NULL || target == NULL ||
                 killer->error || target->error ||
-                killer->holdingID != s->killerWeaponID ) {
+                killer->holdingID != s->killerWeaponID ||
+                target->heldByOther ) {
                 // either player dead, or held-weapon change
+                // or target baby now picked up (safe)
                 
                 // kill request done
                 
