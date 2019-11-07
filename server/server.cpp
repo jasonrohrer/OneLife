d4ceb07b8524fc209a05196ef51d60c57dfd60e6
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-11-07 12:13:07 -0800

    Excess from eating a big food item now goes into bonus overflow food points (same bin as Yum bonus points, with +13 or whatever next to food meter).  Overflow capacity starts at 2 and goes up non-linearly to 91.6, based on base stomach size.  So newborn babies and unfit elderly have 2 overflow capacity, and adults have 91.6 overflow capacity (and more fit elderly have more overflow capacity than 2).  This increases the potential beneficial size range of large foods, like feasts.  The non-linear formuala, based on Miska's suggestion, is 2 + pow( inBaseCapacity, 8 ) * 0.0000000035.

--- server/server.cpp
+++ server/server.cpp
@@ -2685,6 +2685,17 @@ int computeFoodCapacity( LiveObject *inPlayer ) {
 
 
 
+int computeOverflowFoodCapacity( int inBaseCapacity ) {
+    // even littlest baby has +2 overflow, to get everyone used to the
+    // concept.
+    // by adulthood (when base cap is 20), overflow cap is 91.6
+    return 2 + pow( inBaseCapacity, 8 ) * 0.0000000035;
+    }
+
+
+
+
+
 // with 128-wide tiles, character moves at 480 pixels per second
 // at 60 fps, this is 8 pixels per frame
 // important that it's a whole number of pixels for smooth camera movement
@@ -15605,7 +15616,18 @@ int main() {
                                         computeFoodCapacity( nextPlayer );
                                     
                                     if( nextPlayer->foodStore > cap ) {
+    
+                                        int over = nextPlayer->foodStore - cap;
+                                        
                                         nextPlayer->foodStore = cap;
+
+                                        int overflowCap = 
+                                            computeOverflowFoodCapacity( cap );
+
+                                        if( over > overflowCap ) {
+                                            over = overflowCap;
+                                            }
+                                        nextPlayer->yummyBonusStore += over;
                                         }
 
                                     
@@ -16463,7 +16485,18 @@ int main() {
 
                                     
                                     if( targetPlayer->foodStore > cap ) {
+                                        int over = 
+                                            targetPlayer->foodStore - cap;
+                                        
                                         targetPlayer->foodStore = cap;
+
+                                        int overflowCap = 
+                                            computeOverflowFoodCapacity( cap );
+
+                                        if( over > overflowCap ) {
+                                            over = overflowCap;
+                                            }
+                                        targetPlayer->yummyBonusStore += over;
                                         }
                                     targetPlayer->foodDecrementETASeconds =
                                         Time::getCurrentTime() +
