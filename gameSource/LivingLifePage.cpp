204b831174d4920dc8d492105c83a10464f62477
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-11-15 16:37:34 -0800

    Avoid stacking same permanent emot.

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -17417,10 +17417,14 @@ void LivingLifePage::step() {
                             
                             if( ttlSec == -1 ) {
                                 // new permanent emot layer
-                                existing->permanentEmots.push_back(
-                                    getEmotion( emotIndex ) );
-                                
                                 newEmotPlaySound = getEmotion( emotIndex );
+
+                                if( existing->permanentEmots.getElementIndex(
+                                        newEmotPlaySound ) == -1 ) {
+                                    
+                                    existing->permanentEmots.push_back(
+                                        newEmotPlaySound);
+                                    }
                                 }
                             else {
                                 
