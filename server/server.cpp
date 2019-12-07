a8152def1502fd2268aafc9b5a48f50145fad508
Alec Yawata <alec.yawata@gmail.com>
2019-12-07 23:45:39 +0000

    No longer over feeding. Fixes #466

--- server/server.cpp
+++ server/server.cpp
@@ -18290,7 +18290,7 @@ int main() {
                                 // instead, stick to the food cap shown
                                 // in the client (what we last reported
                                 // to them)
-                                int cap = nextPlayer->lastReportedFoodCapacity;
+                                int cap = targetPlayer->lastReportedFoodCapacity;
                                 
 
                                 // first case:
