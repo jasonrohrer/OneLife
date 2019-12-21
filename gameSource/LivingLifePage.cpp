060ab20592a839df5a83169ae607c4d0310d8d86
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-12-20 17:37:01 -0800

    Fixed so that holding a use dummy that is both an ingredient and a target of a recipe doesn't block hint arrows.  Fixes #418

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -6675,17 +6675,34 @@ void LivingLifePage::draw( doublePair inViewCenter,
 
         // are we holding the target of our current hint?
         // if so, hide hint arrows
-        if( mLastHintSortedList.size() > mCurrentHintIndex ) {    
+        if( heldID > 0 &&
+            mLastHintSortedList.size() > mCurrentHintIndex ) {    
+            
             TransRecord *t = 
                 mLastHintSortedList.getElementDirect( mCurrentHintIndex );
             
+            char holdingActor = false;
+            char holdingTarget = false;
+            
+            int heldParent = getObjectParent( heldID );
+
+            if( t->actor > 0 &&
+                getObjectParent( t->actor ) == heldParent ) {
+                holdingActor = true;
+                }
+            if( t->target > 0 &&
+                getObjectParent( t->target ) == heldParent ) {
+                holdingTarget = true;
+                }
+            
+
             
             if( t->newActor > 0 && 
-                t->newActor == heldID  && t->actor != heldID ) {
+                t->newActor == heldID  && ! holdingActor ) {
                 hit = true;
                 }
             else if( t->newTarget > 0 && 
-                     t->newTarget == heldID && t->target != heldID ) {
+                     t->newTarget == heldID && ! holdingTarget ) {
                 hit = true;
                 }
             }
@@ -11462,11 +11479,12 @@ char *LivingLifePage::getHintMessage( int inObjectID, int inIndex,
         mCurrentHintTargetObject[1] = 0;
 
         // never show visual pointer toward what we're holding
-        if( target > 0 && target != inObjectID && 
+        // we handle this elsewhere too, so just obey inDoNotPoint here
+        if( target > 0 && 
             target != inDoNotPointAtThis ) {
             mCurrentHintTargetObject[1] = target;
             }
-        if( actor > 0 && actor != inObjectID &&
+        if( actor > 0 &&
                  actor != inDoNotPointAtThis ) {
             mCurrentHintTargetObject[0] = actor;
             }
