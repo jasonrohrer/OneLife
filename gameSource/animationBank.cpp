c573f3a6943779143fdf609d8709cac899b4cda9
Adk9p <Adk9p@users.noreply.github.com>
2020-01-06 09:22:19 -0800

    Missed a open brace

--- gameSource/animationBank.cpp
+++ gameSource/animationBank.cpp
@@ -2349,7 +2349,7 @@ HoldingPos drawObjectAnim( int inObjectID, int inDrawBehindSlots,
         
         if( obj->person ) {
             if( nudeIndices.getElementIndex(i) != -1 ) {
-                if( !NudeToggle )
+                if( !NudeToggle ) {
                     skipSprite = true;
                 }
             }
