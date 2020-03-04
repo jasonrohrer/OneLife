1d3152fd029271fc55c87322e8dfcf75a23c51ec
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-03-04 12:37:48 -0800

    Container objects that have no sprites in front of contained (like table) now use the lowest-Y sprite in each contained object as the object's center point (other containers use the center of the widest sprite).  This allows objects to sit on the table, centered on their visual bases.  Turkeys and other larger plated objects used to overlap weirdly with other plated objects on tables.

--- gameSource/EditorObjectPage.cpp
+++ gameSource/EditorObjectPage.cpp
@@ -3789,6 +3789,15 @@ void EditorObjectPage::draw( doublePair inViewCenter,
                 }
             }
         else {
+            
+            char allBehind = true;
+            for( int i=0; i< mCurrentObject.numSprites; i++ ) {
+                if( ! mCurrentObject.spriteBehindSlots[i] ) {
+                    allBehind = false;
+                    break;
+                    }
+                }
+
             ObjectRecord *demoObject = getObject( mSlotsDemoObject );
             
             for( int i=0; i<mCurrentObject.numSlots; i++ ) {
@@ -3808,7 +3817,16 @@ void EditorObjectPage::draw( doublePair inViewCenter,
                 setDrawColor( red, 1, blue, alpha );
                 
                 double rot = 0;
-                doublePair centerOffset = getObjectCenterOffset( demoObject );
+
+                doublePair centerOffset;
+
+                if( allBehind ) {
+                    centerOffset = getObjectBottomCenterOffset( demoObject );
+                    }
+                else {
+                    centerOffset = getObjectCenterOffset( demoObject );
+                    }
+
 
                 if( mCurrentObject.slotVert[i] ) {
                     rot = 0.25 + demoObject->vertContainRotationOffset;
