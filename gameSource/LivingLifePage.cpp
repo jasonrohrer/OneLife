9a391e1388ebd742a18eca902708fdc753cb7e64
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-03-12 12:06:21 -0700

    Hunger chimes can now play right up until when final music starts (previously, there was a 23-second gap where music hadn't started playing yet, but hunger chimes were still muted).

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -19608,7 +19608,7 @@ void LivingLifePage::step() {
                         mHungerSlipVisible = 0;
                         }
                     else if( ourLiveObject->foodStore <= 4 &&
-                             computeCurrentAge( ourLiveObject ) < 57 ) {
+                             computeCurrentAge( ourLiveObject ) < 57.33 ) {
                         
                         // don't play hunger sounds at end of life
                         // because it interrupts our end-of-life song
