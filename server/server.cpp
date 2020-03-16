b1a303e9295551d2324aeb64fd261f35fd4d6a9b
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-03-16 11:40:18 -0700

    You get +MORE FOOD+ above your head when an action fails due to hungry work.  Fixes #568

--- server/server.cpp
+++ server/server.cpp
@@ -13756,6 +13756,22 @@ static char isAccessBlocked( LiveObject *inPlayer,
 
 
 
+void sendHungryWorkSpeech( LiveObject *inPlayer ) {
+    // tell player about it with private speech
+    char *message = autoSprintf( 
+        "PS\n"
+        "%d/0 +MORE FOOD+\n#",
+        inPlayer->id );
+    
+    sendMessageToPlayer( 
+        inPlayer, 
+        message, 
+        strlen( message ) );
+    delete [] message;
+    }
+
+
+
 // cost set to 0 unless hungry work not blocked
 char isHungryWorkBlocked( LiveObject *inPlayer, 
                           int inNewTarget, int *outCost ) {          
@@ -19448,6 +19464,8 @@ int main() {
                                             r->newTarget,
                                             &hungryWorkCost ) ) {
                                         r = NULL;
+                                        
+                                        sendHungryWorkSpeech( nextPlayer );
                                         }
                                     }
 
