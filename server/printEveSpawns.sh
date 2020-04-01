480b61324f30f833d9ea04d281bdbae606ddd2b4
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-04-01 09:57:51 -0700

    Script for printing Eve spawn locations on a given day (non-tutorial locations).

---
+++ server/printEveSpawns.sh
@@ -0,0 +1,18 @@
+
+if [ $# -ne 1 ]
+then
+	echo "Usage:"
+	echo "./printEveSpawns.sh lifeLogFile"
+	exit
+fi
+
+
+
+
+while read b time coord
+do
+	
+	date=`date -d @$time`
+
+	echo "$date ||  $coord  "
+done < <( grep noParent $1 | grep -v ",-20" | grep " F " | sed -e "s/\(B [0-9]* \).* F /\1/" | sed "s/ noParent.*//" )
