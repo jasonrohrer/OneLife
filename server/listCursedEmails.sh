fb935a7211eb16cd055950dffd10454e5c9f5af3
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-05-26 12:54:42 -0700

    Fixed syntax.

--- server/listCursedEmails.sh
+++ server/listCursedEmails.sh
@@ -3,4 +3,4 @@
 # whatever
 # a recent test showed this hit all but 48 curses out of 3000+
 
-./curseDBDump | grep "@" | sed "s/ .*//" | sed "s/.com\([0-9a-z]\)/.com \1/i" | sed "s/.co.uk\([0-9a-z]\)/.co.uk \1/i" | sed "s/.org.uk\([0-9a-z]\)/.org.uk \1/i" | grep " " | sed "s/.* //" | sort | uniq -c | sort -n |
+./curseDBDump | grep "@" | sed "s/ .*//" | sed "s/.com\([0-9a-z]\)/.com \1/i" | sed "s/.co.uk\([0-9a-z]\)/.co.uk \1/i" | sed "s/.org.uk\([0-9a-z]\)/.org.uk \1/i" | grep " " | sed "s/.* //" | sort | uniq -c | sort -n
