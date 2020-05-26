9f79ce03a41087baed9d3a79c74b5d106ce79f4b
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-05-26 13:02:02 -0700

    Debug.

--- server/cursesVsMinutesPlayed.sh
+++ server/cursesVsMinutesPlayed.sh
@@ -6,7 +6,7 @@
 find lifeLog/ -type f -mtime -30 -mtime +0 -print0 | xargs -0 grep -h "^D " | sed "s/D [0-9]* [0-9]* //" | sed "s/ [FM] .*//" > tempEmailLives.txt
 
 
-g++ -I../.. -o countCursesAndLives countCursesAndLives.cpp
+g++ -g -I../.. -o countCursesAndLives countCursesAndLives.cpp
 
 
 ./countCursesAndLives
