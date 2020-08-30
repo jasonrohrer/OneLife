3e90d7e6c695c033126f8ec7114bd8b35e478738
risvh <67486979+risvh@users.noreply.github.com>
2020-08-30 22:13:11 +0800

    Oops I reverted change at the wrong line among two similar looking lines

--- gameSource/objectBank.cpp
+++ gameSource/objectBank.cpp
@@ -2503,8 +2503,7 @@ int addObject( const char *inDescription,
 
     int nextObjectNumber = 1;
     
-    if( ! inNoWriteToFile &&
-        objectsDir.exists() && objectsDir.isDirectory() ) {
+    if( objectsDir.exists() && objectsDir.isDirectory() ) {
                 
         File *nextNumberFile = 
             objectsDir.getChildFile( "nextObjectNumber.txt" );
