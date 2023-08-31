0f8f2d1d00f453143a812becf5eda46ab2bc63dd
skps2010 <skps2010030908@yahoo.com>
2023-08-31 21:12:46 +0800

    add remove dependence command

--- scripts/skps2010Scripts/cleanOldBuilds.sh
+++ scripts/skps2010Scripts/cleanOldBuilds.sh
@@ -1,4 +1,5 @@
 find . -type f -name '*.o' -exec rm {} +
 find . -type f -name '*.dep' -exec rm {} +
+rm OneLife/gameSource/Makefile.minorGems_dependencies
 echo 
 echo "Projects cleaned. Ready for compilation."
