45e40214297362dd0eb71d702266e74fc63e4c8c
skps2010 <skps2010030908@yahoo.com>
2023-09-02 22:40:10 +0800

    make it can compile

--- scripts/skps2010Scripts/makeWindows.sh
+++ scripts/skps2010Scripts/makeWindows.sh
@@ -18,6 +18,19 @@ then
     ln -s SDL-1.2.15/include/SDL .
 fi
 
+# freetype lib for linux-windows cross compile
+if [ ! -e mingw32 ]
+then
+    wget https://mirror.msys2.org/mingw/mingw32/mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
+    tar --use-compress-program=unzstd -xvf mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
+    rm mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
+fi
+
+if [ ! -e freetype2 ]
+then
+    ln -s mingw32/include/freetype2 .
+fi
+
 if [ ! -e Winsock.h ]
 then
     ln -s /usr/i686-w64-mingw32/include/winsock.h Winsock.h
