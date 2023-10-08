34579d757047b5233896b8639400ace6ad6182ec
skps2010 <skps2010030908@yahoo.com>
2023-10-09 03:17:17 +0800

    allow to use custom translator & SDL2

--- scripts/skps2010Scripts/makeMacOSX.sh
+++ scripts/skps2010Scripts/makeMacOSX.sh
@@ -7,8 +7,12 @@ fi
 
 ./skps2010Build.sh
 
-python3 -m PyInstaller --onefile translator.py
-cp dist/translator .
+# allow to use custom translator
+if [ ! -e translator ]
+then
+   python3 -m PyInstaller --onefile translator.py
+   cp dist/translator .
+fi
 
 cd OneLife/build
 ./makeDistributionMacOSX v$1 _ /usr/local/opt/sdl12-compat/lib/libSDL-1.2.0.dylib
