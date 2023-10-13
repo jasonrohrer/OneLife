d617c8854eb38a8ba3a0af11682408b111c10880
Jason Rohrer <jasonrohrer@fastmail.fm>
2023-10-13 13:06:56 -0400

    Fixed ambiguity in English.txt file with two orderCommand values defined.  Fixes #950

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -26282,7 +26282,7 @@ void LivingLifePage::keyDown( unsigned char inASCII ) {
                         // actual, spoken text, not a /command
                         
                         if( strstr( typedText,
-                                    translate( "orderCommand" ) ) 
+                                    translate( "orderPrefix" ) ) 
                             == typedText ) {
                             
                             // when issuing an order, place +FOLLOWER+
