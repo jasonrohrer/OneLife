fce93f97a7cbb71ab51def6fd0661eaec372672c
Jason Rohrer <jasonrohrer@fastmail.fm>
2023-11-30 12:03:46 -0500

    Working on expanded export format that supports sound export (whoops) and also bundling multiple exported objects into a single zlib-compressed file.

--- gameSource/exporter.h
+++ gameSource/exporter.h
@@ -1,4 +1,22 @@
 
 
 
+// starts a new export bundle
+// clears/discards any current bundle without saving it
+void initExportBundle();
+
+
+
+// adds an object to the currrent export bundle
+void addExportObject( int inObjectID );
+
+
+// finalizes and saves the bundle as an .oxp or .oxz file in the exports folder
+// depending on settings/compressExports.ini
+void finalizeExportBundle( char *inExportName );
+
+
+
+// FIXME:
+// old function
 void exportObject( int inObjectID );
