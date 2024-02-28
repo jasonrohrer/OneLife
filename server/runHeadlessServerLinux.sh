2791fbe97c504e881ef2a03bab6bd0eaf397ff07
Jason Rohrer <jasonrohrer@fastmail.fm>
2024-02-28 12:47:45 -0500

    Another re-direct in running headless server.

--- server/runHeadlessServerLinux.sh
+++ server/runHeadlessServerLinux.sh
@@ -1,2 +1,2 @@
 ./makeLogRotator
-nohup catchsegv unbuffer ./OneLifeServer | ./logRotator 20000000 serverOut.txt >/dev/null 2>&1 &
+nohup catchsegv unbuffer ./OneLifeServer 2>&1 | ./logRotator 20000000 serverOut.txt >/dev/null 2>&1 &
