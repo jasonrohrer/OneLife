6bda077c99b9c4f6801e3641d94e1ae53ed03162
Jason Rohrer <jasonrohrer@fastmail.fm>
2024-03-05 11:24:27 -0500

    Rsync push script for diff bundles and base binary distributions now works for AHAP files too.

--- scripts/pushDownloadsAndDiffsToMirrors.sh
+++ scripts/pushDownloadsAndDiffsToMirrors.sh
@@ -1,15 +1,29 @@
 while read user server
 do
   echo ""
-  echo "Using rsync to sync all .dbz files to $server"
+  echo "Using rsync to sync all OHOL .dbz files to $server"
   echo ""
 
   rsync -avz -e ssh --progress ~/diffBundles/*.dbz $user@$server:downloads/
+
+  echo ""
+  echo "Using rsync to sync all AHAP .dbz files to $server"
+  echo ""
+
+  rsync -avz -e ssh --progress ~/ahapDiffBundles/*.dbz $user@$server:ahapDownloads/
+
 
   echo ""
   echo "Using rsync to sync all oneLifeDownloads to $server"
   echo ""
 
   rsync -avz --exclude='*.ini' --exclude="*~" -e ssh --progress ~/oneLifeDownloads/* $user@$server:downloads/
+
+
+  echo ""
+  echo "Using rsync to sync all AHAP Downloads to $server"
+  echo ""
+
+  rsync -avz --exclude='*.ini' --exclude="*~" -e ssh --progress ~/ahapDownloads/* $user@$server:ahapDownloads/
 
 done <  <( grep "" ~/diffBundles/remoteServerList.ini )
