c4f5b2c09b10123d56be91d107820a5a63dd5828
Jason Rohrer <jasonrohrer@fastmail.fm>
2024-03-08 15:27:44 -0500

    Script for running scheduled AHAP data updates.

---
+++ scripts/checkForDataUpdateAHAPCron.sh
@@ -0,0 +1,34 @@
+
+
+flagFile=~/ahapDataUpdateFlag.txt
+
+if [ -f $flagFile ]
+then
+
+
+	flag=$(head -n 1 $flagFile)
+
+
+	if [ "$flag" = "1" ]
+	then
+		echo ""
+		echo "Data update scheduled.  Running it now."
+
+		echo "0" > $flagFile
+		
+		date=`date +"%Y_%m_%d__%H_%M_%S"`;
+
+		logPath="/tmp/ahapUpdateGateLog.txt"
+
+		if[ -f $logPath ]; then
+			mv $logPath /tmp/ahapUpdateGateLog_backup_$date.txt
+		fi
+
+		
+
+		# pass two arguments to indicate automation and skip interactive
+		# confirmation
+		~/checkout/OneLifeWorking/scripts/generateDataOnlyDiffBundleAHAP.sh -a -a > $logPath
+				
+	fi
+fi
