63ed14bb7787845ae2d1d7faf2e9dc03a6910a15
Jason Rohrer <jasonrohrer@fastmail.fm>
2024-03-20 15:55:48 -0400

    AHAP Steam Build script now includes snapshot of most recent AHAP content data, so that new users on Steam don't face a huge content download as their initial experience.  Note that as AHAP development progresses, binary updates will be infrequent, so the content included with the Steam binary bundle will lag behind over time.  But at least they won't have to download absolutely every bit of content from scratch (there's already about 50MB of it, just in the starter content alone).

--- scripts/runWindowsSteamDepotBuildAHAP.sh
+++ scripts/runWindowsSteamDepotBuildAHAP.sh
@@ -43,6 +43,97 @@ echo -n "http://onehouronelife.com/ahapSteamGate/server.php" > steamLatestAHAP/s
 
 
 
-echo "Running steamcmd to push AHAP Windows build"
 
-steamcmd +login "jasonrohrergames" +run_app_build -desc AnotherPlanetClient_windows_v$1 ~/checkout/OneLifeWorking/build/steam/ahap/app_build_windows_ON_SERVER_2787060.vdf +quit | tee /tmp/steamBuildLogAHAP.txt
+echo "" 
+echo "Preparing to bundle latest AHAP data with AHAP Windows Steam Build"
+echo ""
+
+
+
+echo "" 
+echo "Updating OneLifeWorking"
+echo ""
+
+cd ~/checkout/OneLifeWorking
+git pull --tags
+
+
+echo "" 
+echo "Building headless cache generation tool"
+echo ""
+
+cd gameSource
+sh ./makeRegenerateCaches
+
+
+
+
+echo "" 
+echo "Updating AnotherPlanetDataLatest"
+echo ""
+
+cd ~/checkout/AnotherPlanetDataLatest
+git pull --tags
+
+
+
+lastTaggedDataVersion=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/AnotherPlanet_v* | sed -e 's/AnotherPlanet_v//'`
+
+
+echo "" 
+echo "Most recent Data git version is:  $lastTaggedDataVersion"
+echo ""
+
+baseDataVersion=$lastTaggedDataVersion
+
+
+rm -rf ~/checkout/ahapDataTemp
+
+
+echo "" 
+echo "Exporting base data for bundling"
+echo ""
+
+git checkout -q AnotherPlanet_v$baseDataVersion
+
+git clone . ~/checkout/ahapDataTemp
+rm -rf ~/checkout/ahapDataTemp/.git*
+rm ~/checkout/ahapDataTemp/.hg*
+rm -rf ~/checkout/ahapDataTemp/soundsRaw
+rm -rf ~/checkout/ahapDataTemp/faces
+rm -rf ~/checkout/ahapDataTemp/scenes
+rm -r ~/checkout/ahapDataTemp/*.sh ~/checkout/ahapDataTemp/working ~/checkout/ahapDataTemp/overlays
+
+
+# restore repository to latest, to avoid confusion later
+git checkout -q master
+
+
+cp -r ~/checkout/ahapDataTemp/* ~/steamLatestAHAP
+
+rm -rf ~/checkout/ahapDataTemp
+
+
+
+cd ~/steamLatestAHAP
+
+echo "" 
+echo "Generating caches for content"
+echo ""
+
+
+cp ~/checkout/OneLifeWorking/gameSource/reverbImpulseResponse.aiff .
+~/checkout/OneLifeWorking/gameSource/regenerateCaches
+rm reverbImpulseResponse.aiff
+# don't include bin_cache files in downloads, because they change
+# with each update, and are too big
+# cache.fcz files are full of compressed text files, so they're much smaller
+# and fine to included when they change
+rm */bin_*cache.fcz
+
+
+
+
+echo "Running steamcmd to push AHAP Windows v$1 build, including v$baseDataVersion content."
+
+steamcmd +login "jasonrohrergames" +run_app_build -desc AnotherPlanetClient_windows__Bin_v$1__Content_v$baseDataVersion ~/checkout/OneLifeWorking/build/steam/ahap/app_build_windows_ON_SERVER_2787060.vdf +quit | tee /tmp/steamBuildLogAHAP.txt
