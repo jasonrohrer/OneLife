9f4d61f07e94e3f882043c6de772c55599dc3e90
Jason Rohrer <jasonrohrer@fastmail.fm>
2024-05-17 15:53:25 -0400

    Crontab to delete old log files (foodLog, foodLogDetail, failureLog), since they were taking up a lot of disk space over the years.  Now deleting logs over 60 days on all servers.

---
+++ scripts/clearOldLogFiles.sh
@@ -0,0 +1,13 @@
+cd ~/checkout/OneLife/server/foodLog
+
+find . -type f -mtime +60 -name '*.txt' -delete
+
+
+cd ~/checkout/OneLife/server/foodLogDetail
+
+find . -type f -mtime +60 -name '*.txt' -delete
+
+
+cd ~/checkout/OneLife/server/failureLog
+
+find . -type f -mtime +60 -name '*.txt' -delete
