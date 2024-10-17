b8f31efeba7d403764947a51f0c40cdfb69457e1
Jason Rohrer <jasonrohrer@fastmail.fm>
2024-10-17 13:47:31 -0400

    Copied antiDOS script from skydrop, but adjusted slot number in ufw from 3 to 5.

---
+++ scripts/antiDOS.sh
@@ -0,0 +1,26 @@
+# run as root
+
+tail -n 200000 /var/log/nginx/access.log | sed "s/ -.*//" | sort | uniq -c | sort -n | \
+while read line ; do
+	# split into parts
+	parts=($line)
+	count=${parts[0]}
+	ip=${parts[1]}
+	
+
+	if [[ $count -gt 10000 ]];
+	then
+		echo "blocking future connections from $ip"
+		echo "since it has made $count requests recently"
+		
+		alreadyBlocked="$( ufw status | grep -c $ip )"
+
+		if [[ $alreadyBlocked -eq 0 ]];
+		then
+			ufw insert 5 deny from $ip to any port 443
+			ufw insert 5 deny from $ip to any port 80
+		else
+			echo "already blocked by ufw, doing nothing."
+		fi
+	fi
+done
