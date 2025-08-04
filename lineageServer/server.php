74bdadcf97a4ef8abb94882abd3c7255a293af02
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-08-03 23:48:17 -0400

    Whoops, left custom filter set when there are too many name matches, preventing showing only Eves for deep roots, making query very slow.

--- lineageServer/server.php
+++ lineageServer/server.php
@@ -1641,6 +1641,7 @@ function ls_frontPage() {
         if( $numNameMatches > 1000 ) {
             $filterClause = " WHERE 1 ";
             $tooManyNameMatches = true;
+            $customFilterSet = false;
             }
         else {
             // not too many
