cc252cb56668f22b77b526e7ffd2051e01c9196c
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-10-21 16:22:25 -0400

    HungryWork now correctly applies to floor objects.

--- server/server.cpp
+++ server/server.cpp
@@ -16761,6 +16761,31 @@ char isHungryWorkBlocked( LiveObject *inPlayer,
     }
 
 
+void applyHungryWorkCost( LiveObject *inPlayer, int inHungryWorkCost ) {
+    if( inHungryWorkCost > 0 ) {
+        if( inPlayer->yummyBonusStore > 0 ) {
+            if( inPlayer->yummyBonusStore
+                >= inHungryWorkCost ) {
+                inPlayer->yummyBonusStore -=
+                    inHungryWorkCost;
+                inHungryWorkCost = 0;
+                }
+            else {
+                inHungryWorkCost -= 
+                    inPlayer->yummyBonusStore;
+                inPlayer->yummyBonusStore = 0;
+                }
+            }
+                                        
+        inPlayer->foodStore -= inHungryWorkCost;
+                                        
+        // we checked above, so player
+        // never is taken down below 5 here
+        inPlayer->foodUpdate = true;
+        }
+    }
+
+
 
 const char *numberToWords( int inNumber ) {
     switch( inNumber ) {
@@ -25096,27 +25121,8 @@ int main( int inNumArgs, const char **inArgs ) {
                                             }
                                         }
                                     
-                                    if( hungryWorkCost > 0 ) {
-                                        if( nextPlayer->yummyBonusStore > 0 ) {
-                                            if( nextPlayer->yummyBonusStore
-                                                >= hungryWorkCost ) {
-                                                nextPlayer->yummyBonusStore -=
-                                                    hungryWorkCost;
-                                                hungryWorkCost = 0;
-                                                }
-                                            else {
-                                                hungryWorkCost -= 
-                                                    nextPlayer->yummyBonusStore;
-                                                nextPlayer->yummyBonusStore = 0;
-                                                }
-                                            }
-                                        
-                                        nextPlayer->foodStore -= hungryWorkCost;
-                                        
-                                        // we checked above, so player
-                                        // never is taken down below 5 here
-                                        nextPlayer->foodUpdate = true;
-                                        }
+                                    applyHungryWorkCost( nextPlayer,
+                                                         hungryWorkCost );
                                     
                                     
                                     setResponsiblePlayer( -1 );
@@ -25500,6 +25506,21 @@ int main( int inNumArgs, const char **inArgs ) {
                                             floorID );
                                         }
                                         
+                                    int hungryWorkCost = 0;
+                                
+                                    if( r != NULL && 
+                                        r->newTarget > 0 ) {
+
+                                        if( isHungryWorkBlocked( 
+                                                nextPlayer,
+                                                r->newTarget,
+                                                &hungryWorkCost ) ) {
+                                            r = NULL;
+                                        
+                                            sendHungryWorkSpeech( nextPlayer );
+                                            }
+                                        }
+                                    
 
                                     if( r != NULL && 
                                         // make sure we're not too young
@@ -25564,6 +25585,8 @@ int main( int inNumArgs, const char **inArgs ) {
                                                 usedOnFloor = true;
                                                 }
                                             }
+                                        applyHungryWorkCost( nextPlayer,
+                                                             hungryWorkCost );
                                         }
                                     }
                                 
