fa557df666b90a381ca913557ce2b63a554c4cf1
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-11-04 13:22:18 -0500

    ahapGate now keeps same content leader as before in the event of a tie vote.  Fixes #1196

--- ahapGate/server.php
+++ ahapGate/server.php
@@ -1812,6 +1812,19 @@ function ag_getUserIDForGithubUsername( $inGithub ) {
 function ag_updateContentLeader() {
 
     global $tableNamePrefix;
+
+    $query = "SELECT content_leader_github ".
+        "FROM $tableNamePrefix"."server_globals;";
+    
+    $result = ag_queryDatabase( $query );
+    $numRows = mysqli_num_rows( $result );
+
+    $prevLeader = "";
+
+    if( $numRows > 0 ) {
+        $prevLeader = ag_mysqli_result( $result, 0, 0 );
+        }
+    
     
     $query =
         "SELECT COUNT(*), content_leader_github_vote ".
@@ -1857,17 +1870,24 @@ function ag_updateContentLeader() {
     else {
         // tie for multiple leaders
 
-        // pick one with lowest ID
+        if( $prevLeader != "" ) {
+            // previous leader wins, stays in office
+            $bestLeader = $prevLeader;
+            }
+        else {
+            // no previous leader 
+            // pick one with lowest ID
 
-        $lowestID = 999999999;
-        $bestLeader = "";
+            $lowestID = 999999999;
+            $bestLeader = "";
 
-        foreach( $bestLeaders as $l ) {
-            $id = ag_getUserIDForGithubUsername( $l );
+            foreach( $bestLeaders as $l ) {
+                $id = ag_getUserIDForGithubUsername( $l );
 
-            if( $id < $lowestID ) {
-                $bestLeader = $l;
-                $lowestID = $id;
+                if( $id < $lowestID ) {
+                    $bestLeader = $l;
+                    $lowestID = $id;
+                    }
                 }
             }
         }
