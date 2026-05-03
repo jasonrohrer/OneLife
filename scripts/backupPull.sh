3ac74c421daa44f6144a7d4fa4899f02278d3d05
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-05-03 11:50:32 -0400

    Updated backup server pull script to pull from new linode servers for The Castle Doctrine and Cordial Minuet

--- scripts/backupPull.sh
+++ scripts/backupPull.sh
@@ -82,6 +82,58 @@ find ~/backups/$server -mtime +14 -delete
 
 
 
+
+user=jcr13
+server=thecastledoctrine.onehouronelife.com
+
+
+echo ""
+echo "Using rsync to sync all backups from $server"
+echo ""
+
+
+if [ ! -f ~/backups/$server ]
+then
+    mkdir ~/backups/$server
+fi
+
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress $user@$server:backups/* ~/backups/$server
+
+
+echo ""
+echo "Deleting local files that are more than 14 days old"
+echo ""
+
+# delete backup files older than two weeks
+find ~/backups/$server -mtime +14 -delete
+
+
+
+
+user=jcr13
+server=cordialminuet.onehouronelife.com
+
+
+echo ""
+echo "Using rsync to sync all backups from $server"
+echo ""
+
+
+if [ ! -f ~/backups/$server ]
+then
+    mkdir ~/backups/$server
+fi
+
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress $user@$server:backups/* ~/backups/$server
+
+
+echo ""
+echo "Deleting local files that are more than 14 days old"
+echo ""
+
+# delete backup files older than two weeks
+find ~/backups/$server -mtime +14 -delete
+
  
 
 
@@ -94,30 +146,6 @@ fi
 
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress northcountrynotes.org:backups/* ~/backups/northcountrynotes/
 
-
-
-if [ ! -f ~/backups/thecastledoctrine ]
-then
-    mkdir ~/backups/thecastledoctrine
-fi
-
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress thecastledoctrine.net:backups/* ~/backups/thecastledoctrine/
-
-# delete backup files older than two weeks
-find ~/backups/thecastledoctrine -mtime +14 -delete
-
-
-
-if [ ! -f ~/backups/cordialminuet ]
-then
-    mkdir ~/backups/cordialminuet
-fi
-
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress cordialminuet.com:backups/* ~/backups/cordialminuet/
-
-
-# delete backup files older than two weeks
-find ~/backups/cordialminuet -mtime +14 -delete
 
 
 
